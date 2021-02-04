edc163baebb6337cfb59b34b6e7393a9033ad71d
leopph69 <levente.loffler99@gmail.com>
2021-02-04 10:20:55 +0100

    Added basic math library.

--- Client/src/client.cpp
+++ Client/src/client.cpp
@@ -2,6 +2,7 @@
 #include <vector.h>
 #include <matrix.h>
 #include <iostream>
+#include <leopphmath.h>
 
 using leopph::Vector3;
 using leopph::Matrix3;
@@ -10,5 +11,6 @@ using leopph::Matrix4;
 
 void leopph::Init()
 {
-	
+	std::cout << leopph::Math::ToDegrees(2 * leopph::Math::Pi()) << std::endl;
+	std::cout << leopph::Math::ToRadians(180) << std::endl;
 }
