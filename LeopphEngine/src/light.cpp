058db2b7d8a05bd132d518380f10bbc6b89d32ae
leopph69 <levente.loffler99@gmail.com>
2021-02-05 09:30:59 +0100

    Initial light class.

---
+++ LeopphEngine/src/light.cpp
@@ -0,0 +1,47 @@
+#include "light.h"
+
+namespace leopph
+{
+	namespace implementation
+	{
+		// init static members
+		std::set<Light*> Light::s_PointLights{};
+		Light* Light::s_DirectionalLight{};
+
+
+		// getters
+		const std::set<Light*>& Light::PointLights()
+		{
+			return s_PointLights;
+		}
+
+		Light* Light::DirectionalLight()
+		{
+			return s_DirectionalLight;
+		}
+
+
+
+		// registration
+		void Light::RegisterDirectionalLight(Light* light)
+		{
+			s_DirectionalLight = light;
+		}
+
+		void Light::RegisterPointLight(Light* light)
+		{
+			s_PointLights.insert(light);
+		}
+
+
+		void Light::UnregisterDirectionalLight(Light* light)
+		{
+			s_DirectionalLight = nullptr;
+		}
+
+		void Light::UnregisterPointLight(Light* light)
+		{
+			s_PointLights.erase(light);
+		}
+	}
+}
