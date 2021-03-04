57c2d8366a9fe6de3bdc254b9149d40703e89f2a
Löffler Levente <levente.loffler99@gmail.com>
2021-03-04 12:18:38 +0100

    Changed cpp version to C++20. Replaced vector and matrix template constraints with concepts. Fixed matrix multiplication dimension constraints.

--- LeopphEngine/src/include/matrix.h
+++ LeopphEngine/src/include/matrix.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <type_traits>
 #include <cstddef>
+#include <concepts>
 
 #include "vector.h"
 #include "leopphmath.h"
@@ -10,340 +10,375 @@
 
 namespace leopph
 {
-	template<class T, size_t N, size_t M>
-	class Matrix
+	namespace implementation
 	{
-		static_assert(N >= 2 && M >= 2, "Matrix must be at least 2x2!");
-
-
-	private:
-		Vector<T, M> m_Data[N];
-
-
-	public:
-		// constructors
-		Matrix();
-		Matrix(const T& value);
-		Matrix(const Matrix<T, N, M>& other);
-
-		template<class... T1, std::enable_if_t<std::conjunction_v<std::is_convertible<T1, T>...> && sizeof...(T1) == (N > M ? M : N), bool> = false>
-		Matrix(const T1&... args) :
-			m_Data{}
+		template<class T, std::size_t N, std::size_t M> requires(N > 1 && M > 1)
+		class Matrix
 		{
-			T argArr[M > N ? N : M]{ static_cast<T>(args)... };
-
-			for (size_t i = 0; i < M && i < N; i++)
-				m_Data[i][i] = argArr[i];
-		}
-
-		template<class... T1, std::enable_if_t<std::conjunction_v<std::is_convertible<T1, T>...> && sizeof...(T1) == (N * M), bool> = false>
-		Matrix(const T1&... args) :
-			m_Data{}
-		{
-			T argArr[M * N]{ static_cast<T>(args)... };
-
-			for (size_t i = 0; i < N; i++)
-				for (size_t j = 0; j < M; j++)
-					m_Data[i][j] = argArr[i * M + j];
-		}
-
-		template<std::size_t N1 = (M > N ? N : M), std::enable_if_t<N1 == (M > N ? N : M), bool> = false>
-		Matrix(const Vector<T, N1>& vec) :
-			m_Data{}
-		{
-			for (size_t i = 0; i < N1; i++)
-				m_Data[i][i] = vec[i];
-		}
+		private:
+			Vector<T, M> m_Data[N];
 
 
+		public:
+			// constructors
+			Matrix() :
+				m_Data{}
+			{}
 
-
-
-
-		// factories
-		template<size_t N1 = N, size_t M1 = M, std::enable_if_t<N1 == M1 && N1 == N && M1 == M, bool> = false>
-		static Matrix<T, N, M> Identity()
-		{
-			return Matrix<T, N, M>{1};
-		}
-
-
-
-
-
-
-		// lookat matrix
-		template<size_t N1 = N, size_t M1 = M, std::enable_if_t<N1 == M1 && N1 == N && M1 == M && N1 == 4, bool> = false>
-		static Matrix<T, 4, 4> LookAt(const Vector<T, 3>& position, const Vector<T, 3>& target, const Vector<T, 3>& worldUp)
-		{
-			Vector<T, 3> z{ (target - position).Normalized() };
-			Vector<T, 3> x{ Vector<T, 3>::Cross(worldUp, z).Normalized() };
-			Vector<T, 3> y{ Vector<T, 3>::Cross(z, x) };
-
-			return Matrix<T, 4, 4>
+			Matrix(const T& value) :
+				m_Data{}
 			{
-				x[0],		y[0],		z[0],		0,
-				x[1],		y[1],		z[1],		0,
-				x[2],		y[2],		z[2],		0,
-				-Vector3::Dot(x, position),		-Vector3::Dot(y, position),		-Vector3::Dot(z, position),		1
-			};
-		}
+				for (size_t i = 0; i < N && i < M; i++)
+					m_Data[i][i] = value;
+			}
 
-
-
-
-
-
-		// PERSPECTIVE PROJECTION
-		template<size_t N1 = N, size_t M1 = M, class = std::enable_if_t<N1 == M1 && N1 == N && M1 == M && N1 == 4>>
-		static Matrix<T, 4, 4> Perspective(const T& left, const T& right, const T& top, const T& bottom, const T& nearClipPlane, const T& farClipPlane)
-		{
-			Matrix<T, 4, 4> ret;
-
-			ret[0][0] = (static_cast<T>(2) * nearClipPlane) / (right - left);
-			ret[2][0] = (right + left) / (right - left);
-			ret[1][1] = (static_cast<T>(2) * nearClipPlane) / (top - bottom);
-			ret[2][1] = (top + bottom) / (top - bottom);
-			ret[2][2] = (farClipPlane + nearClipPlane) / (farClipPlane - nearClipPlane);
-			ret[2][3] = static_cast<T>(1);
-			ret[3][2] = (static_cast<T>(-2) * farClipPlane * nearClipPlane) / (farClipPlane - nearClipPlane);
-
-			return ret;
-
-		}
-
-
-		template<size_t N1 = N, size_t M1 = M, class = std::enable_if_t<N1 == M1 && N1 == N && M1 == M && N1 == 4>>
-		static Matrix<T, 4, 4> Perspective(const T& fov, const T& aspectRatio, const T& nearClipPlane, const T& farClipPlane)
-		{
-			T tanHalfFov{ static_cast<T>(Math::Tan(fov / static_cast<T>(2))) };
-			T top{ nearClipPlane * tanHalfFov };
-			T bottom{ -top };
-			T right{ top * aspectRatio };
-			T left{ -right };
-
-			return Perspective(left, right, top, bottom, nearClipPlane, farClipPlane);
-		}
-
-
-
-
-
-
-
-
-
-		// translation matrix
-		template<size_t N1 = N, size_t M1 = M, std::enable_if_t<N1 == M1 && N1 == N && M1 == M && N1 == 4, bool> = false>
-		static Matrix<T, 4, 4> Translate(const Vector<T, 3>& vector)
-		{
-			Matrix<T, 4, 4> ret = Identity();
-
-			for (size_t i = 0; i < 3; i++)
-				ret[3][i] = vector[i];
-
-			return ret;
-		}
-
-
-		// scale matrix
-		template<size_t N1 = N, size_t M1 = M, std::enable_if_t<N1 == M1 && N1 == N && M1 == M && N1 == 4, bool> = false>
-		static Matrix<T, 4, 4> Scale(const Vector<T, 3>& vector)
-		{
-			Matrix<T, 4, 4> ret = Identity();
-
-			for (size_t i = 0; i < 3; i++)
-				ret[i][i] = vector[i];
-
-			ret[3][3] = 1;
-
-			return ret;
-		}
-
-
-
-
-
-		// get stored data as pointer
-		const T* Data() const;
-		T* Data();
-
-
-
-
-
-		// member operators
-		Matrix<T, N, M>& operator=(const Matrix<T, N, M>& other);
-		const Vector<T, M>& operator[](size_t index) const;
-		Vector<T, M>& operator[](size_t index);
-
-
-
-
-
-		// determinant
-		template<size_t N1 = N, size_t M1 = M, std::enable_if_t<N1 == M1 && N1 == N && M1 == M, bool> = false>
-		float Det() const
-		{
-			Matrix<T, N, M> tmp{ *this };
-
-			for (size_t i = 1; i < N; i++)
-				for (size_t j = 0; j < N; j++)
-					tmp[j][0] += tmp[j][i];
-
-			for (size_t i = 1; i < N; i++)
-				tmp[i] -= tmp[0];
-
-			float ret{ 1.0f };
-
-			for (size_t i = 0; i < N; i++)
-				ret *= tmp[i][i];
-
-			return ret;
-		}
-
-
-
-
-
-		// TRANSPOSE
-		Matrix<T, M, N> Transposed() const;
-
-		template<size_t N1 = N, size_t M1 = M, std::enable_if_t<N1 == N && M1 == M && N1 == M1, bool> = false>
-		Matrix<T, N, M> Transpose()
-		{
-			Matrix<T, N, M> transposed = Transposed();
-
-			for (size_t i = 0; i < N; i++)
-				for (size_t j = 0; j < M; j++)
-					m_Data[i][j] = transposed[i][j];
-
-			return *this;
-		}
-
-
-
-
-		// INVERSE
-		template<size_t N1 = N, size_t M1 = M, std::enable_if_t<N1 == M1 && N1 == N && M1 == M, bool> = false>
-		Matrix<T, N, M> Inverse() const
-		{
-			Matrix<T, N, M> copyOfThis{ *this };
-			Matrix<T, N, M> inverse{ Matrix<T, N, M>::Identity() };
-
-			for (std::size_t i = 0; i < N; i++)
+			Matrix(const Matrix<T, N, M>& other) :
+				m_Data{}
 			{
-				if (copyOfThis[i][i] != static_cast<T>(1) && copyOfThis[i][i] != static_cast<T>(0))
+				for (size_t i = 0; i < N; i++)
+					m_Data[i] = other.m_Data[i];
+			}
+
+			template<std::convertible_to<T>... T1> requires(sizeof...(T1) == (N > M ? M : N))
+				Matrix(const T1&... args) :
+				m_Data{}
+			{
+				T argArr[M > N ? N : M]{ static_cast<T>(args)... };
+
+				for (size_t i = 0; i < M && i < N; i++)
+					m_Data[i][i] = argArr[i];
+			}
+
+			template<std::convertible_to<T> ... T1> requires(sizeof...(T1) == (N * M))
+				Matrix(const T1&... args) :
+				m_Data{}
+			{
+				T argArr[M * N]{ static_cast<T>(args)... };
+
+				for (size_t i = 0; i < N; i++)
+					for (size_t j = 0; j < M; j++)
+						m_Data[i][j] = argArr[i * M + j];
+			}
+
+			template<std::size_t N1> requires(N1 == (M > N ? N : M))
+				Matrix(const Vector<T, N1>& vec) :
+				m_Data{}
+			{
+				for (size_t i = 0; i < N1; i++)
+					m_Data[i][i] = vec[i];
+			}
+
+
+
+
+
+
+			// factories
+			static Matrix<T, N, M> Identity() requires(N == M)
+			{
+				return Matrix<T, N, M>{1};
+			}
+
+
+
+
+
+
+			// lookat matrix
+			static Matrix<T, 4, 4> LookAt(const Vector<T, 3>& position, const Vector<T, 3>& target, const Vector<T, 3>& worldUp) requires(N == 4 && M == 4)
+			{
+				Vector<T, 3> z{ (target - position).Normalized() };
+				Vector<T, 3> x{ Vector<T, 3>::Cross(worldUp, z).Normalized() };
+				Vector<T, 3> y{ Vector<T, 3>::Cross(z, x) };
+
+				return Matrix<T, 4, 4>
 				{
-					T mult = copyOfThis[i][i];
-					for (std::size_t k = 0; k < N; k++)
-					{
-						copyOfThis[i][k] /= mult;
-						inverse[i][k] /= mult;
-					}
-				}
-					
-				for (std::size_t j = 0; j < N; j++)
-					if (j != i)
-					{
-						T mult = copyOfThis[j][i] / copyOfThis[i][i];
-						copyOfThis[j] -= mult * copyOfThis[i];
-						inverse[j] -= mult * inverse[i];
-					}
+					x[0], y[0], z[0], 0,
+						x[1], y[1], z[1], 0,
+						x[2], y[2], z[2], 0,
+						-Vector3::Dot(x, position), -Vector3::Dot(y, position), -Vector3::Dot(z, position), 1
+				};
 			}
 
-			return inverse;
+
+
+
+
+
+			// PERSPECTIVE PROJECTION
+			static Matrix<T, 4, 4> Perspective(const T& left, const T& right, const T& top, const T& bottom, const T& nearClipPlane, const T& farClipPlane) requires (N == 4 && M == 4)
+			{
+				Matrix<T, 4, 4> ret;
+
+				ret[0][0] = (static_cast<T>(2) * nearClipPlane) / (right - left);
+				ret[2][0] = (right + left) / (right - left);
+				ret[1][1] = (static_cast<T>(2) * nearClipPlane) / (top - bottom);
+				ret[2][1] = (top + bottom) / (top - bottom);
+				ret[2][2] = (farClipPlane + nearClipPlane) / (farClipPlane - nearClipPlane);
+				ret[2][3] = static_cast<T>(1);
+				ret[3][2] = (static_cast<T>(-2) * farClipPlane * nearClipPlane) / (farClipPlane - nearClipPlane);
+
+				return ret;
+
+			}
+
+
+			static Matrix<T, 4, 4> Perspective(const T& fov, const T& aspectRatio, const T& nearClipPlane, const T& farClipPlane)  requires (N == 4 && M == 4)
+			{
+				T tanHalfFov{ static_cast<T>(Math::Tan(fov / static_cast<T>(2))) };
+				T top{ nearClipPlane * tanHalfFov };
+				T bottom{ -top };
+				T right{ top * aspectRatio };
+				T left{ -right };
+
+				return Perspective(left, right, top, bottom, nearClipPlane, farClipPlane);
+			}
+
+
+
+
+
+
+
+
+
+			// translation matrix
+			static Matrix<T, 4, 4> Translate(const Vector<T, 3>& vector) requires (N == 4 && M == 4)
+			{
+				Matrix<T, 4, 4> ret = Identity();
+
+				for (size_t i = 0; i < 3; i++)
+					ret[3][i] = vector[i];
+
+				return ret;
+			}
+
+
+			// scale matrix
+			static Matrix<T, 4, 4> Scale(const Vector<T, 3>& vector) requires (N == 4 && M == 4)
+			{
+				Matrix<T, 4, 4> ret = Identity();
+
+				for (size_t i = 0; i < 3; i++)
+					ret[i][i] = vector[i];
+
+				ret[3][3] = 1;
+
+				return ret;
+			}
+
+
+
+
+
+			// get stored data as pointer
+			const T* Data() const
+			{
+				return m_Data[0].Data();
+			}
+			T* Data()
+			{
+				return const_cast<T*>(const_cast<const Matrix<T, N, M>*>(this)->Data());
+			}
+
+
+
+
+
+			// member operators
+			Matrix<T, N, M>& operator=(const Matrix<T, N, M>& other)
+			{
+				if (this == &other)
+					return *this;
+
+				for (size_t i = 0; i < N; i++)
+					for (size_t j = 0; j < M; j++)
+						m_Data[i][j] = other.m_Data[i][j];
+
+				return *this;
+			}
+
+			const Vector<T, M>& operator[](size_t index) const
+			{
+				return m_Data[index];
+			}
+			Vector<T, M>& operator[](size_t index)
+			{
+				return const_cast<Vector<T, M>&>(const_cast<const Matrix<T, N, M>*>(this)->operator[](index));
+			}
+
+
+
+
+
+			// determinant
+			float Det() const requires(N == M)
+			{
+				Matrix<T, N, M> tmp{ *this };
+
+				for (size_t i = 1; i < N; i++)
+					for (size_t j = 0; j < N; j++)
+						tmp[j][0] += tmp[j][i];
+
+				for (size_t i = 1; i < N; i++)
+					tmp[i] -= tmp[0];
+
+				float ret{ 1.0f };
+
+				for (size_t i = 0; i < N; i++)
+					ret *= tmp[i][i];
+
+				return ret;
+			}
+
+
+
+
+
+			// TRANSPOSE
+			Matrix<T, M, N> Transposed() const
+			{
+				Matrix<T, M, N> ret;
+
+				for (size_t i = 0; i < N; i++)
+					for (size_t j = 0; j < M; j++)
+						ret[j][i] = m_Data[i][j];
+
+				return ret;
+			}
+
+			Matrix<T, N, M> Transpose() requires(N == M)
+			{
+				Matrix<T, N, M> transposed = Transposed();
+
+				for (size_t i = 0; i < N; i++)
+					for (size_t j = 0; j < M; j++)
+						m_Data[i][j] = transposed[i][j];
+
+				return *this;
+			}
+
+
+
+
+			// INVERSE
+			Matrix<T, N, M> Inverse() const requires(N == M)
+			{
+				Matrix<T, N, M> copyOfThis{ *this };
+				Matrix<T, N, M> inverse{ Matrix<T, N, M>::Identity() };
+
+				for (std::size_t i = 0; i < N; i++)
+				{
+					if (copyOfThis[i][i] != static_cast<T>(1) && copyOfThis[i][i] != static_cast<T>(0))
+					{
+						T mult = copyOfThis[i][i];
+						for (std::size_t k = 0; k < N; k++)
+						{
+							copyOfThis[i][k] /= mult;
+							inverse[i][k] /= mult;
+						}
+					}
+
+					for (std::size_t j = 0; j < N; j++)
+						if (j != i)
+						{
+							T mult = copyOfThis[j][i] / copyOfThis[i][i];
+							copyOfThis[j] -= mult * copyOfThis[i];
+							inverse[j] -= mult * inverse[i];
+						}
+				}
+
+				return inverse;
+			}
+
+
+
+			operator Matrix<T, N - 1, N - 1>() requires(N == M)
+			{
+				Matrix<T, N - 1, N - 1> ret{};
+
+				for (std::size_t i = 0; i < N - 1; i++)
+					for (std::size_t j = 0; j < N - 1; j++)
+						ret[i][j] = m_Data[i][j];
+
+				return ret;
+			}
+		};
+
+
+
+
+
+		// non member operators
+		template<class T, std::size_t N, std::size_t M>
+		std::ostream& operator<<(std::ostream& stream, const Matrix<T, N, M>& matrix)
+		{
+			for (size_t i = 0; i < N; i++)
+			{
+				for (size_t j = 0; j < M; j++)
+				{
+					stream << matrix[i][j];
+
+					if (j != M - 1)
+						stream << " ";
+				}
+
+				if (i != N - 1)
+					stream << std::endl;
+			}
+
+			return stream;
 		}
 
 
-		
 
-		template<std::size_t N1 = N, std::size_t M1 = M, std::enable_if_t<N1 == M1 && N1 == N && M1 == M, bool> = false>
-		operator Matrix<T, N - 1, N - 1>()
+		template<class T, std::size_t N, std::size_t M, std::size_t P>
+		Matrix<T, N, P> operator*(const Matrix<T, N, M>& left, const Matrix<T, M, P>& right)
 		{
-			Matrix<T, N - 1, N - 1> ret{};
+			Matrix<T, N, P> ret;
 
-			for (std::size_t i = 0; i < N - 1; i++)
-				for (std::size_t j = 0; j < N - 1; j++)
-					ret[i][j] = m_Data[i][j];
+			for (size_t i = 0; i < N; i++)
+				for (size_t j = 0; j < P; j++)
+					for (size_t k = 0; k < M; k++)
+						ret[i][j] += left[i][k] * right[k][j];
 
 			return ret;
 		}
-	};
 
 
 
-
-
-	// non member operators
-	template<class T, size_t N, size_t M>
-	std::ostream& operator<<(std::ostream& stream, const Matrix<T, N, M>& matrix)
-	{
-		for (size_t i = 0; i < N; i++)
+		template<class T, std::size_t N, std::size_t M>
+		Matrix<T, N, M>& operator*=(Matrix<T, N, M>& left, const Matrix<T, M, M>& right)
 		{
-			for (size_t j = 0; j < M; j++)
-			{
-				stream << matrix[i][j];
-
-				if (j != M - 1)
-					stream << " ";
-			}
-
-			if (i != N - 1)
-				stream << std::endl;
+			return left = left * right;
 		}
 
-		return stream;
-	}
 
 
+		template<class T, std::size_t N, std::size_t M>
+		Vector<T, M> operator*(const Vector<T, N>& left, const Matrix<T, N, M>& right)
+		{
+			Vector<T, M> ret;
 
-	template<class T, size_t N1, size_t M1, size_t N2, size_t M2, std::enable_if_t<M1 == N2, bool> = false>
-	Matrix<T, N1, M2> operator*(const Matrix<T, N1, M1>& left, const Matrix<T, N2, M2>& right)
-	{
-		Matrix<T, N1, M2> ret;
+			for (size_t j = 0; j < M; j++)
+				for (size_t k = 0; k < N; k++)
+					ret[j] += left[k] * right[k][j];
 
-		for (size_t i = 0; i < N1; i++)
-			for (size_t j = 0; j < M2; j++)
-				for (size_t k = 0; k < M1; k++)
-					ret[i][j] += left[i][k] * right[k][j];
-
-		return ret;
-	}
+			return ret;
+		}
 
 
+		template<class T, std::size_t N, std::size_t M>
+		Vector<T, N> operator*(const Matrix<T, N, M>& left, const Vector<T, M>& right)
+		{
+			Vector<T, N> ret;
 
-	template<class T, size_t N1, size_t M1, size_t N2, size_t M2, std::enable_if_t<M1 == N2 && N1 == M1 && N2 == M2, bool> = false>
-	Matrix<T, N1, M1>& operator*=(Matrix<T, N1, M1>& left, const Matrix<T, N2, M2>& right)
-	{
-		return left = left * right;
-	}
+			for (size_t i = 0; i < N; i++)
+				for (size_t k = 0; k < M; k++)
+					ret[i] += left[i][k] * right[k];
 
-
-
-	template<class T, size_t P, size_t N, size_t M, std::enable_if_t<P == N, bool> = false>
-	Vector<T, M> operator*(const Vector<T, P>& left, const Matrix<T, N, M>& right)
-	{
-		Vector<T, M> ret;
-
-		for (size_t j = 0; j < M; j++)
-			for (size_t k = 0; k < P; k++)
-				ret[j] += left[k] * right[k][j];
-
-		return ret;
-	}
-
-
-	template<class T, size_t N, size_t M, size_t P, std::enable_if_t<M == P, bool> = false>
-	Vector<T, N> operator*(const Matrix<T, N, M>& left, const Vector<T, P>& right)
-	{
-		Vector<T, N> ret;
-
-		for (size_t i = 0; i < N; i++)
-			for (size_t k = 0; k < M; k++)
-				ret[i] += left[i][k] * right[k];
-
-		return ret;
+			return ret;
+		}
 	}
 
 
@@ -351,7 +386,7 @@ namespace leopph
 
 
 	// aliases
-	using Matrix2 = Matrix<float, 2, 2>;
-	using Matrix3 = Matrix<float, 3, 3>;
-	using Matrix4 = Matrix<float, 4, 4>;
+	using Matrix2 = implementation::Matrix<float, 2, 2>;
+	using Matrix3 = implementation::Matrix<float, 3, 3>;
+	using Matrix4 = implementation::Matrix<float, 4, 4>;
 }
--- LeopphEngine/src/include/vector.h
+++ LeopphEngine/src/include/vector.h
@@ -1,259 +1,344 @@
 #pragma once
 
-#include "leopphapi.h"
-
 #include <type_traits>
 #include <ostream>
 #include <cstddef>
+#include <concepts>
 
 
 namespace leopph
 {
-	template<class T, size_t N>
-	class LEOPPHAPI Vector
+	namespace implementation
 	{
-		static_assert(N >= 2, "Vector dimension must be at least 2!");
-
-
-	private:
-		T m_Data[N];
-
-
-	public:
-		// constructors
-		Vector();
-		Vector(const T& value);
-		Vector(const Vector<T, N>& other);
-
-		template<class... T1, std::enable_if_t<std::conjunction_v<std::is_convertible<T1, T>...> && sizeof...(T1) == N, bool> = false>
-		Vector(const T1&... args) :
-			m_Data{ static_cast<T>(args)... }
-		{}
-
-
-
-
-
-		// quick access factories
-		static Vector<T, N> Up();
-		static Vector<T, N> Down();
-		static Vector<T, N> Left();
-		static Vector<T, N> Right();
-
-		template<size_t N1 = N, std::enable_if_t<N1 >= 3 && N1 == N, bool> = false>
-		static Vector<T, N> Forward()
+		template<class T, std::size_t N> requires(N > 1)
+		class Vector
 		{
-			Vector<T, N> ret;
-			ret[2] = 1;
+		private:
+			T m_Data[N];
+
+
+		public:
+			// constructors
+			Vector() :
+				m_Data{}
+			{}
+
+			Vector(const T& value) :
+				m_Data{}
+			{
+				for (size_t i = 0; i < N; i++)
+					m_Data[i] = value;
+			}
+
+			Vector(const Vector<T, N>& other) :
+				m_Data{}
+			{
+				for (size_t i = 0; i < N; i++)
+					m_Data[i] = other.m_Data[i];
+			}
+
+			template<std::convertible_to<T>... T1>
+			Vector(const T1&... args) requires(sizeof...(T1) == N) :
+				m_Data{ static_cast<T>(args)... }
+			{}
+
+
+
+
+
+			// quick access factories
+			static Vector<T, N> Up()
+			{
+				Vector<T, N> ret;
+				ret[1] = 1;
+				return ret;
+			}
+			static Vector<T, N> Down()
+			{
+				Vector<T, N> ret;
+				ret[1] = -1;
+				return ret;
+			}
+			static Vector<T, N> Left()
+			{
+				Vector<T, N> ret;
+				ret[0] = -1;
+				return ret;
+			}
+			static Vector<T, N> Right()
+			{
+				Vector<T, N> ret;
+				ret[0] = 1;
+				return ret;
+			}
+			static Vector<T, N> Forward() requires(N >= 3)
+			{
+				Vector<T, N> ret;
+				ret[2] = 1;
+				return ret;
+			}
+			static Vector<T, N> Backward() requires(N >= 3)
+			{
+				Vector<T, N> ret;
+				ret[2] = -1;
+				return ret;
+			}
+
+
+
+
+
+			// get stored values as pointer
+			const T* Data() const
+			{
+				return &m_Data[0];
+			}
+			T* Data()
+			{
+				return const_cast<T*>(const_cast<const Vector<T, N>*>(this)->Data());
+			}
+
+
+
+
+
+			// member operators
+			Vector<T, N>& operator=(const Vector<T, N>& other)
+			{
+				if (this == &other)
+					return *this;
+
+				for (size_t i = 0; i < N; i++)
+					m_Data[i] = other.m_Data[i];
+
+				return *this;
+			}
+
+			const T& operator[](size_t index) const
+			{
+				return m_Data[index];
+			}
+			T& operator[](size_t index)
+			{
+				return const_cast<T&>(const_cast<const Vector<T, N>*>(this)->operator[](index));
+			}
+
+
+
+
+			// magnitude
+			float Length() const
+			{
+				float sqrSum{};
+
+				for (size_t i = 0; i < N; i++)
+					sqrSum += std::powf(m_Data[i], 2);
+
+				return std::sqrtf(sqrSum);
+			}
+
+
+
+
+			// normalization
+			Vector<T, N>& Normalize()
+			{
+				float length = Length();
+
+				for (size_t i = 0; i < N; i++)
+					m_Data[i] /= length;
+
+				return *this;
+			}
+			Vector<T, N> Normalized() const
+			{
+				return Vector<T, N>{ *this }.Normalize();
+			}
+
+
+
+
+
+			// dot product
+			static T Dot(const Vector<T, N>& left, const Vector<T, N>& right)
+			{
+				T ret{};
+
+				for (size_t i = 0; i < N; i++)
+					ret += left[i] * right[i];
+
+				return ret;
+			}
+
+
+
+
+			// cross product, only for 3d vector
+			static Vector<T, N> Cross(const Vector<T, N>& left, const Vector<T, N>& right) requires(N == 3)
+			{
+				return Vector<T, N> {	left[1] * right[2] - left[2] * right[1],
+					left[2] * right[0] - left[0] * right[2],
+					left[0] * right[1] - left[1] * right[0] };
+			}
+
+
+			// distance
+			static float Distance(const Vector<T, N>& left, const Vector<T, N>& right)
+			{
+				T sum{};
+
+				for (size_t i = 0; i < N; i++)
+					sum += static_cast<T>(std::powf(static_cast<float>(left[i] - right[i]), 2));
+
+				return static_cast<T>(std::sqrt(sum));
+			}
+		};
+
+
+
+
+
+
+		// non member operators
+		template<class T, std::size_t N>
+		Vector<T, N> operator-(const Vector<T, N>& operand)
+		{
+			Vector<T, N> ret{ operand };
+
+			for (size_t i = 0; i < N; i++)
+				ret[i] = -ret[i];
+
 			return ret;
 		}
 
-		template<size_t N1 = N, std::enable_if_t<N1 >= 3 && N1 == N, bool> = false>
-		static Vector<T, N> Backward()
+
+
+
+		template<class T, std::size_t N>
+		Vector<T, N> operator+(const Vector<T, N>& left, const Vector<T, N>& right)
 		{
 			Vector<T, N> ret;
-			ret[2] = -1;
+
+			for (size_t i = 0; i < N; i++)
+				ret[i] = left[i] + right[i];
+
 			return ret;
 		}
 
-
-
-
-
-		// get stored values as pointer
-		const T* Data() const;
-		T* Data();
-
-
-
-
-
-		// member operators
-		Vector<T, N>& operator=(const Vector<T, N>& other);
-		const T& operator[](size_t index) const;
-		T& operator[](size_t index);
-
-
-
-
-		// magnitude
-		float Length() const;
-
-
-
-
-		// normalization
-		Vector<T, N> Normalized() const;
-		Vector<T, N>& Normalize();
-
-
-
-
-
-		// dot product
-		static T Dot(const Vector<T, N>& left, const Vector<T, N>& right);
-
-
-
-
-		// cross product, only for 3d vector
-		template<size_t N1 = N, std::enable_if_t<N1 == 3 && N1 == N, bool> = false>
-		static Vector<T, N> Cross(const Vector<T, N>& left, const Vector<T, N>& right)
+		template<class T, std::size_t N>
+		Vector<T, N> operator+=(Vector<T, N>& left, const Vector<T, N>& right)
 		{
-			return Vector<T, N> {	left[1] * right[2] - left[2] * right[1],
-				left[2] * right[0] - left[0] * right[2],
-				left[0] * right[1] - left[1] * right[0] };
+			return left = left + right;
 		}
 
 
-		// distance
-		static float Distance(const Vector<T, N>& left, const Vector<T, N>& right);
-	};
 
 
-
-
-
-
-	// non member operators
-	template<class T, size_t N>
-	Vector<T, N> operator-(const Vector<T, N>& operand)
-	{
-		Vector<T, N> ret{ operand };
-
-		for (size_t i = 0; i < N; i++)
-			ret[i] = -ret[i];
-
-		return ret;
-	}
-
-
-
-
-	template<class T, size_t N>
-	Vector<T, N> operator+(const Vector<T, N>& left, const Vector<T, N>& right)
-	{
-		Vector<T, N> ret;
-
-		for (size_t i = 0; i < N; i++)
-			ret[i] = left[i] + right[i];
-
-		return ret;
-	}
-
-	template<class T, size_t N>
-	Vector<T, N> operator+=(Vector<T, N>& left, const Vector<T, N>& right)
-	{
-		return left = left + right;
-	}
-
-
-
-
-	template<class T, size_t N>
-	Vector<T, N> operator-(const Vector<T, N>& left, const Vector<T, N>& right)
-	{
-		return left + -right;
-	}
-
-	template<class T, size_t N>
-	Vector<T, N> operator-=(Vector<T, N>& left, const Vector<T, N>& right)
-	{
-		return left = left - right;
-	}
-
-
-
-
-	template<class T1, class T2, size_t N, std::enable_if_t<std::is_convertible_v<T2, T1>, bool> = false>
-	Vector<T1, N> operator*(const Vector<T1, N>& left, const T2& right)
-	{
-		Vector<T1, N> ret{ left };
-
-		for (size_t i = 0; i < N; i++)
-			ret[i] *= right;
-
-		return ret;
-	}
-
-	template<class T1, class T2, size_t N, std::enable_if_t<std::is_convertible_v<T2, T1>, bool> = false>
-	Vector<T1, N> operator*(const T2& left, const Vector<T1, N>& right)
-	{
-		return right * left;
-	}
-
-	template<class T1, class T2, size_t N, std::enable_if_t<std::is_convertible_v<T2, T1>, bool> = false>
-	Vector<T1, N> operator*=(Vector<T1, N>& left, const T2& right)
-	{
-		return left = left * right;
-	}
-
-	template<class T1, class T2, size_t N, std::enable_if_t<std::is_convertible_v<T2, T1>, bool> = false>
-	Vector<T1, N> operator*=(const T2& left, Vector<T1, N>& right)
-	{
-		return right = left * right;
-	}
-
-
-
-
-	template<class T1, class T2, size_t N, std::enable_if_t<std::is_convertible_v<T2, T1>, bool> = false>
-	Vector<T1, N> operator/(const Vector<T1, N>& left, const T2& right)
-	{
-		return left * (static_cast<T1>(1) / right);
-	}
-
-	template<class T1, class T2, size_t N, std::enable_if_t<std::is_convertible_v<T2, T1>, bool> = false>
-	Vector<T1, N> operator/=(Vector<T1, N>& left, const T2& right)
-	{
-		return left = left * (static_cast<T1>(1) / right);
-	}
-
-
-
-
-	template<class T, size_t N>
-	bool operator==(const Vector<T, N>& left, const Vector<T, N>& right)
-	{
-		for (size_t i = 0; i < N; i++)
-			if (left[i] != right[i])
-				return false;
-
-		return true;
-	}
-
-	template<class T, size_t N>
-	bool operator!=(const Vector<T, N>& left, const Vector<T, N>& right)
-	{
-		return !(left == right);
-	}
-
-
-
-
-	template<class T, size_t N>
-	std::ostream& operator<<(std::ostream& stream, const Vector<T, N>& vector)
-	{
-		stream << "(";
-
-		for (size_t i = 0; i < N; i++)
+		template<class T, std::size_t N>
+		Vector<T, N> operator-(const Vector<T, N>& left, const Vector<T, N>& right)
 		{
-			stream << vector[i];
-
-			if (i != N - 1)
-				stream << ", ";
+			return left + -right;
 		}
 
-		stream << ")";
+		template<class T, std::size_t N>
+		Vector<T, N> operator-=(Vector<T, N>& left, const Vector<T, N>& right)
+		{
+			return left = left - right;
+		}
 
-		return stream;
+
+
+
+		template<class T1, std::convertible_to<T1> T2, std::size_t N>
+		Vector<T1, N> operator*(const Vector<T1, N>& left, const T2& right)
+		{
+			Vector<T1, N> ret{ left };
+
+			for (size_t i = 0; i < N; i++)
+				ret[i] *= right;
+
+			return ret;
+		}
+
+		template<class T1, std::convertible_to<T1> T2, std::size_t N>
+		Vector<T1, N> operator*(const T2& left, const Vector<T1, N>& right)
+		{
+			return right * left;
+		}
+
+		template<class T1, std::convertible_to<T1> T2, std::size_t N>
+		Vector<T1, N> operator*=(Vector<T1, N>& left, const T2& right)
+		{
+			return left = left * right;
+		}
+
+		template<class T1, std::convertible_to<T1> T2, std::size_t N>
+		Vector<T1, N> operator*=(const T2& left, Vector<T1, N>& right)
+		{
+			return right = left * right;
+		}
+
+
+
+
+		template<class T1, std::convertible_to<T1> T2, std::size_t N>
+		Vector<T1, N> operator/(const Vector<T1, N>& left, const T2& right)
+		{
+			return left * (static_cast<T1>(1) / right);
+		}
+
+		template<class T1, std::convertible_to<T1> T2, std::size_t N>
+		Vector<T1, N> operator/=(Vector<T1, N>& left, const T2& right)
+		{
+			return left = left * (static_cast<T1>(1) / right);
+		}
+
+
+
+
+		template<class T, std::size_t N>
+		bool operator==(const Vector<T, N>& left, const Vector<T, N>& right)
+		{
+			for (size_t i = 0; i < N; i++)
+				if (left[i] != right[i])
+					return false;
+
+			return true;
+		}
+
+		template<class T, std::size_t N>
+		bool operator!=(const Vector<T, N>& left, const Vector<T, N>& right)
+		{
+			return !(left == right);
+		}
+
+
+
+
+		template<class T, std::size_t N>
+		std::ostream& operator<<(std::ostream& stream, const Vector<T, N>& vector)
+		{
+			stream << "(";
+
+			for (size_t i = 0; i < N; i++)
+			{
+				stream << vector[i];
+
+				if (i != N - 1)
+					stream << ", ";
+			}
+
+			stream << ")";
+
+			return stream;
+		}
 	}
 
 
 
-	
+
 	// aliasing
-	using Vector4 = Vector<float, 4>;
-	using Vector3 = Vector<float, 3>;
-	using Vector2 = Vector<float, 2>;
+	using Vector4 = implementation::Vector<float, 4>;
+	using Vector3 = implementation::Vector<float, 3>;
+	using Vector2 = implementation::Vector<float, 2>;
 }
