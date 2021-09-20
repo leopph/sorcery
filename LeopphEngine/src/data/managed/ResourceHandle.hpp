e8f43eb3e4772ff33ac6ebc60943ae9b0b0a0442
Löffler Levente <levente.loffler99@gmail.com>
2021-09-20 10:16:42 +0200

    Fix ResourceHandle leaking internal headers

--- LeopphEngine/src/data/managed/ResourceHandle.hpp
+++ LeopphEngine/src/data/managed/ResourceHandle.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "../DataManager.hpp"
 #include "Resource.hpp"
 #include "ResourceHandleBase.hpp"
 #include "UniqueResource.hpp"
@@ -9,99 +8,84 @@
 #include <filesystem>
 #include <utility>
 
-// TODO this leaks lots of headers
 
 
 namespace leopph::impl
 {
 	template<typename T>
 		requires std::derived_from<T, Resource> || std::derived_from<T, UniqueResource>
-		class ResourceHandle : public ResourceHandleBase
+	class ResourceHandle : public ResourceHandleBase
 	{
-	public:
-		explicit ResourceHandle(auto&&... args)
-			requires std::derived_from<T, Resource> :
-			m_ResPtr{ new T{ std::forward<decltype(args)>(args)... } }, resource{ m_ResPtr }
-		{
-			Init();
-		}
-
-
-		explicit ResourceHandle(std::filesystem::path path)
-			requires std::derived_from<T, UniqueResource> :
-		m_ResPtr{ static_cast<T*>(DataManager::Find(path)) }, resource{ m_ResPtr }
-		{
-			if (m_ResPtr == nullptr)
+		public:
+			explicit ResourceHandle(auto&&... args)
+				requires std::derived_from<T, Resource> :
+				m_ResPtr{new T{std::forward<decltype(args)>(args)...}},
+				resource{m_ResPtr}
 			{
-				m_ResPtr = new T{ std::forward<std::filesystem::path>(path) };
+				Init(m_ResPtr);
 			}
 
-			Init();
-		}
 
-
-		ResourceHandle(const ResourceHandle& other) :
-			m_ResPtr{ other.m_ResPtr }, resource{ m_ResPtr }
-		{
-			Init();
-		}
-
-
-		ResourceHandle(ResourceHandle&& other) noexcept :
-			ResourceHandle{ other }
-		{}
-
-
-		ResourceHandle& operator=(const ResourceHandle& other)
-		{
-			if (&other != this)
+			explicit ResourceHandle(std::filesystem::path path)
+				requires std::derived_from<T, UniqueResource> :
+				m_ResPtr{static_cast<T*>(Find(path))},
+				resource{m_ResPtr}
 			{
-				Deinit();
-				m_ResPtr = other.m_ResPtr;
-				Init();
+				if (m_ResPtr == nullptr)
+				{
+					m_ResPtr = new T{std::forward<std::filesystem::path>(path)};
+				}
+
+				Init(m_ResPtr);
 			}
-			return *this;
-		}
 
 
-		ResourceHandle& operator=(ResourceHandle&& other) noexcept
-		{
-			return this->operator=(other);
-		}
-
-
-		~ResourceHandle() override
-		{
-			Deinit();
-		}
-
-
-	private:
-		T* m_ResPtr;
-
-
-		void Init()
-		{
-			DataManager::Register(m_ResPtr, this);
-		}
-
-
-		void Deinit()
-		{
-			DataManager::Unregister(m_ResPtr, this);
-
-			if (DataManager::Count(m_ResPtr) == 0)
+			ResourceHandle(const ResourceHandle& other) :
+				m_ResPtr{other.m_ResPtr},
+				resource{m_ResPtr}
 			{
-				delete m_ResPtr;
+				Init(m_ResPtr);
 			}
-		}
 
 
-	protected:
-		T* const& resource;
+			ResourceHandle(ResourceHandle&& other) noexcept :
+				ResourceHandle{other}
+			{}
 
 
-		friend class Resource;
-		friend class UniqueResource;
+			ResourceHandle& operator=(const ResourceHandle& other)
+			{
+				if (&other != this)
+				{
+					Deinit(m_ResPtr);
+					m_ResPtr = other.m_ResPtr;
+					Init(m_ResPtr);
+				}
+				return *this;
+			}
+
+
+			ResourceHandle& operator=(ResourceHandle&& other) noexcept
+			{
+				return this->operator=(other);
+			}
+
+
+			~ResourceHandle() override
+			{
+				Deinit(m_ResPtr);
+			}
+
+
+		private:
+			T* m_ResPtr;
+
+
+		protected:
+			T* const& resource;
+
+
+			friend class Resource;
+			friend class UniqueResource;
 	};
 }
--- LeopphEngine/src/data/managed/ResourceHandleBase.hpp
+++ LeopphEngine/src/data/managed/ResourceHandleBase.hpp
@@ -1,16 +1,32 @@
 #pragma once
 
+#include "Resource.hpp"
+#include "UniqueResource.hpp"
+
+#include <filesystem>
+
 
 namespace leopph::impl
 {
 	class ResourceHandleBase
 	{
 	public:
+		virtual ~ResourceHandleBase();
+
+
+		protected:
 		ResourceHandleBase() = default;
 		ResourceHandleBase(const ResourceHandleBase& other) = default;
 		ResourceHandleBase(ResourceHandleBase&& other) = default;
 		ResourceHandleBase& operator=(const ResourceHandleBase& other) = default;
 		ResourceHandleBase& operator=(ResourceHandleBase&& other) = default;
-		virtual ~ResourceHandleBase() = default;
+
+		static UniqueResource* Find(const std::filesystem::path& path);
+
+		void Init(const Resource* resource) const;
+		void Init(const UniqueResource* uniqueResource) const;
+
+		void Deinit(const Resource* resource) const;
+		void Deinit(const UniqueResource* uniqueResource) const;
 	};
 }
