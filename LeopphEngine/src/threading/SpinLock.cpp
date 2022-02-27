cc4a1737694651e0c2b9427ca7408ec8c281f61a
Löffler Levente <levente.loffler99@gmail.com>
2022-02-27 08:37:15 +0100

    Add SpinLock

---
+++ LeopphEngine/src/threading/SpinLock.cpp
@@ -0,0 +1,23 @@
+#include "SpinLock.hpp"
+
+#include <intrin.h>
+
+
+namespace leopph::internal
+{
+	auto SpinLock::Lock() noexcept -> void
+	{
+		// Poll the lock while it is locked
+		while (m_Lock.test_and_set(std::memory_order_acquire))
+		{
+			// PAUSE instruction to slow cpu down while spinning
+			_mm_pause();
+		}
+	}
+
+
+	auto SpinLock::Unlock() noexcept -> void
+	{
+		m_Lock.clear(std::memory_order_release);
+	}
+}
