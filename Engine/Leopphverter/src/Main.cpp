0159f34f7557cdc60b9ecd7b44da6315b93b5daf
leopph <levente.loffler99@gmail.com>
2022-06-17 00:35:17 +0200

    Fix test driver for deserializaton

--- Engine/Leopphverter/src/Main.cpp
+++ Engine/Leopphverter/src/Main.cpp
@@ -9,9 +9,10 @@ auto static constexpr DST_FILE_PATH = R"#(C:\Users\leven\Desktop\test.leopph3d)#
 
 auto main() -> int
 {
-	//auto const model = leopph::convert::Import(SRC_FILE_PATH);
-	//auto bytes = leopph::convert::Export(model, std::endian::little);
-	//std::ofstream out{DST_FILE_PATH, std::ios::binary | std::ios::out};
-	//out.write(reinterpret_cast<char const*>(bytes.data()), sizeof(decltype(bytes)::value_type) * bytes.size());
+	auto const model = leopph::convert::Import(SRC_FILE_PATH);
+	auto bytes = leopph::convert::Export(model, std::endian::little);
+	std::ofstream out{DST_FILE_PATH, std::ios::binary | std::ios::out};
+	out.setf(std::ios_base::unitbuf);
+	out.write(reinterpret_cast<char const*>(bytes.data()), sizeof(decltype(bytes)::value_type) * bytes.size());
 	auto const parsedModel = leopph::convert::Import(DST_FILE_PATH);
 }
