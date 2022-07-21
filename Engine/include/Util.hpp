36cb609f698e88ccba204bc44bd1ffec6962adf6
leopph <levente.loffler99@gmail.com>
2022-07-21 10:53:36 +0200

    Add ClampCast

--- Engine/include/Util.hpp
+++ Engine/include/Util.hpp
@@ -2,10 +2,16 @@
 
 #include "LeopphApi.hpp"
 
+#include <algorithm>
+#include <concepts>
 #include <filesystem>
+#include <limits>
 #include <string>
 #include <string_view>
+#include <utility>
 #include <vector>
+
+
 
 namespace leopph
 {
@@ -15,4 +21,36 @@ namespace leopph
 
 	auto LEOPPHAPI SplitLines(std::string_view str, std::vector<std::string>& out) -> void;
 	auto LEOPPHAPI SplitWords(std::string_view const view, std::vector<std::string_view>& out) -> void;
+
+	template<std::integral To, std::integral From>
+	[[nodiscard]] auto ClampCast(From const from) -> To;
+}
+
+
+
+template<std::integral To, std::integral From>
+auto leopph::ClampCast(From const from) -> To
+{
+	if constexpr (std::cmp_less_equal(std::numeric_limits<To>::min(), std::numeric_limits<From>::min()))
+	{
+		if constexpr (std::cmp_greater_equal(std::numeric_limits<To>::max(), std::numeric_limits<From>::max()))
+		{
+			return static_cast<To>(from);
+		}
+		else
+		{
+			return static_cast<To>(std::min<From>(from, std::numeric_limits<To>::max()));
+		}
+	}
+	else
+	{
+		if constexpr (std::cmp_greater_equal(std::numeric_limits<To>::max(), std::numeric_limits<From>::max()))
+		{
+			return static_cast<To>(std::max<From>(from, std::numeric_limits<To>::min()));
+		}
+		else
+		{
+			return static_cast<To>(std::clamp<From>(from, std::numeric_limits<To>::min(), std::numeric_limits<To>::max()));
+		}
+	}
 }
