3073c7ba1e7e29f6cd4be9bc3f1bb87486111d66
leopph <levente.loffler99@gmail.com>
2022-11-08 18:10:21 +0100

    Make field and property parsing culture-independent

--- Engine/projects/RuntimeManaged/NativeHelpers.cs
+++ Engine/projects/RuntimeManaged/NativeHelpers.cs
@@ -1,13 +1,15 @@
 ﻿using System;
+using System.Globalization;
 using System.Reflection;
 
 namespace leopph
 {
 	internal static class NativeHelpers
 	{
-		private static readonly object[] s_singleObjectHolder = new object[1];
-		private static readonly Type[] s_singleTypeHolder = new Type[1];
+		private static readonly object[] s_ObjectArrayTwoElems = new object[2];
+		private static readonly Type[] s_TypeArrayTwoElems = new Type[2];
 		private static readonly Type s_stringType = typeof(string);
+		private static readonly Type s_cultureInfoType = typeof(CultureInfo);
 
 		internal static bool ShouldSerializeField(FieldInfo field)
 		{
@@ -94,22 +96,26 @@ namespace leopph
 
 		internal static object ParseFieldValue(FieldInfo field, string value)
 		{
-			s_singleObjectHolder[0] = value;
-			s_singleTypeHolder[0] = s_stringType;
+			s_ObjectArrayTwoElems[0] = value;
+			s_ObjectArrayTwoElems[1] = CultureInfo.InvariantCulture;
+			s_TypeArrayTwoElems[0] = s_stringType;
+			s_TypeArrayTwoElems[1] = s_cultureInfoType;
 
-			var parseMethod = field.FieldType.GetMethod("Parse", s_singleTypeHolder);
-			var parsedValue = parseMethod.Invoke(null, s_singleObjectHolder);
+			var parseMethod = field.FieldType.GetMethod("Parse", s_TypeArrayTwoElems);
+			var parsedValue = parseMethod.Invoke(null, s_ObjectArrayTwoElems);
 			return parsedValue;
 		}
 
 
 		internal static object ParsePropertyValue(PropertyInfo property, string value)
 		{
-			s_singleObjectHolder[0] = value;
-			s_singleTypeHolder[0] = s_stringType;
+			s_ObjectArrayTwoElems[0] = value;
+			s_ObjectArrayTwoElems[1] = CultureInfo.InvariantCulture;
+			s_TypeArrayTwoElems[0] = s_stringType;
+			s_TypeArrayTwoElems[1] = s_cultureInfoType;
 
-			var parseMethod = property.GetMethod.ReturnType.GetMethod("Parse", s_singleTypeHolder);
-			var parsedValue = parseMethod.Invoke(null, s_singleObjectHolder);
+			var parseMethod = property.GetMethod.ReturnType.GetMethod("Parse", s_TypeArrayTwoElems);
+			var parsedValue = parseMethod.Invoke(null, s_ObjectArrayTwoElems);
 			return parsedValue;
 		}
 
