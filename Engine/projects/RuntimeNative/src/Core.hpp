b8acd44f87b9d43544414c2a96afdc2eec526d80
Levente Löffler <levente.loffler99@gmail.com>
2022-12-22 18:03:07 +0100

    Fix editor not compiling by removing #ifdef _WINDLL from LEOPPHAPI definition

--- Engine/projects/RuntimeNative/src/Core.hpp
+++ Engine/projects/RuntimeNative/src/Core.hpp
@@ -4,14 +4,10 @@
 #error "Only 64 bit Windows is supported."
 #endif
 
-#ifdef _WINDLL
 #ifdef LEOPPH_NATIVE_RUNTIME_EXPORT
 #define LEOPPHAPI __declspec(dllexport)
 #else
 #define LEOPPHAPI __declspec(dllimport)
-#endif
-#else
-#define LEOPPHAPI
 #endif
 
 
