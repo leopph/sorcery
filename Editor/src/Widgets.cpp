2a45f8663e3006c3a684263f899bb1131da13e4c
Levente Löffler <levente.loffler99@gmail.com>
2023-01-30 12:27:10 +0100

    Fix warnings in DrawSpinner

--- Editor/src/Widgets.cpp
+++ Editor/src/Widgets.cpp
@@ -5,48 +5,49 @@
 #include <cmath>
 
 namespace ImGui {
-	bool Spinner(const char* label, float radius, int thickness, const ImU32& color) {
-		ImGuiWindow* window = GetCurrentWindow();
-		if (window->SkipItems)
-			return false;
-
-		ImGuiContext& g = *GImGui;
-		const ImGuiStyle& style = g.Style;
-		const ImGuiID id = window->GetID(label);
-
-		ImVec2 pos = window->DC.CursorPos;
-		ImVec2 size((radius) * 2, (radius + style.FramePadding.y) * 2);
-
-		const ImRect bb(pos, ImVec2(pos.x + size.x, pos.y + size.y));
-		ItemSize(bb, style.FramePadding.y);
-		if (!ItemAdd(bb, id))
-			return false;
-
-		// Render
-		window->DrawList->PathClear();
-
-		int num_segments = 30;
-		int start = abs(ImSin(g.Time * 1.8f) * (num_segments - 5));
-
-		const float a_min = IM_PI * 2.0f * ((float)start) / (float)num_segments;
-		const float a_max = IM_PI * 2.0f * ((float)num_segments - 3) / (float)num_segments;
-
-		const ImVec2 centre = ImVec2(pos.x + radius, pos.y + radius + style.FramePadding.y);
-
-		for (int i = 0; i < num_segments; i++) {
-			const float a = a_min + ((float)i / (float)num_segments) * (a_max - a_min);
-			window->DrawList->PathLineTo(ImVec2(centre.x + ImCos(a + g.Time * 8) * radius,
-												centre.y + ImSin(a + g.Time * 8) * radius));
-		}
-
-		window->DrawList->PathStroke(color, false, thickness);
+auto Spinner(char const* label, float const radius, int const thickness, ImU32 const& color) -> bool {
+	ImGuiWindow* window = GetCurrentWindow();
+	if (window->SkipItems) {
+		return false;
 	}
+
+	auto const& g{ *GImGui };
+	ImGuiStyle const& style = g.Style;
+	ImGuiID const id{ window->GetID(label) };
+
+	auto const pos{ window->DC.CursorPos };
+	ImVec2 const size{ (radius) * 2, (radius + style.FramePadding.y) * 2 };
+
+	ImRect const bb{ pos, ImVec2(pos.x + size.x, pos.y + size.y) };
+	ItemSize(bb, style.FramePadding.y);
+	if (!ItemAdd(bb, id)) {
+		return false;
+	}
+
+	// Render
+	window->DrawList->PathClear();
+
+	int constexpr num_segments{ 30 };
+	float const start{ std::abs(ImSin(static_cast<float>(g.Time) * 1.8f) * (num_segments - 5)) };
+
+	float const a_min = IM_PI * 2.0f * static_cast<float>(start) / static_cast<float>(num_segments);
+	float constexpr a_max = IM_PI * 2.0f * (static_cast<float>(num_segments) - 3) / static_cast<float>(num_segments);
+
+	auto const centre = ImVec2(pos.x + radius, pos.y + radius + style.FramePadding.y);
+
+	for (int i = 0; i < num_segments; i++) {
+		float const a = a_min + static_cast<float>(i) / static_cast<float>(num_segments) * (a_max - a_min);
+		window->DrawList->PathLineTo(ImVec2{ centre.x + ImCos(a + static_cast<float>(g.Time) * 8) * radius, centre.y + ImSin(a + static_cast<float>(g.Time) * 8) * radius });
+	}
+
+	window->DrawList->PathStroke(color, false, static_cast<float>(thickness));
+	return true;
+}
 }
 
 
-
 namespace leopph::editor {
-	auto DrawSpinner(char const* const label, float const radius, int const thickness, ImU32 const& color) -> bool {
-		return ImGui::Spinner(label, radius, thickness, color);
-	}
+auto DrawSpinner(char const* const label, float const radius, int const thickness, ImU32 const& color) -> bool {
+	return ImGui::Spinner(label, radius, thickness, color);
+}
 }
