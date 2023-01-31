0b100d1794f3f9da7b60a41ece49c82c1134f7fc
Levente Löffler <levente.loffler99@gmail.com>
2023-01-31 20:26:51 +0100

    Silence warning in ToneMapGammaPS.hlsl about negative pow base

--- RuntimeNative/src/shaders/ToneMapGammaPS.hlsl
+++ RuntimeNative/src/shaders/ToneMapGammaPS.hlsl
@@ -11,7 +11,11 @@ float4 main(const VsOut vsOut) : SV_TARGET {
     float3 pixelColor = gHdrTexture.Sample(gSamplerState, vsOut.uv).xyz;
 
     pixelColor = pixelColor / (pixelColor + 1.0);
+#pragma warning(push)
+	// warns about negative pow base, irrelevant here as pixel color should never be negative
+#pragma warning(disable: 3571)
     pixelColor = pow(pixelColor, invGamma);
+#pragma warning(pop)
 
     return float4(pixelColor, 1);
 }
