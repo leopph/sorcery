66377d814a403504e190c60b0c6608d0b4db4b9f
Levente Löffler <levente.loffler99@gmail.com>
2023-02-01 15:05:46 +0100

    Add intrinsics for operator/= on Vector3

--- RuntimeNative/src/Math.hpp
+++ RuntimeNative/src/Math.hpp
@@ -228,6 +228,11 @@ template<>
 [[nodiscard]] inline auto operator/(float left, Vector3 const& right) noexcept -> Vector3;
 template<>
 [[nodiscard]] inline auto operator/(Vector3 const& left, Vector3 const& right) noexcept -> Vector3;
+
+template<>
+inline auto operator/=(Vector3& left, float right) noexcept -> Vector3&;
+template<>
+inline auto operator/=(Vector3& left, Vector3 const& right) noexcept -> Vector3&;
 #endif
 
 
@@ -1102,6 +1107,28 @@ inline auto operator/(Vector3 const& left, Vector3 const& right) noexcept -> Vec
 	Vector3 ret;
 	_mm_maskstore_ps(ret.GetData(), memMask, xmm2);
 	return ret;
+}
+
+
+template<>
+inline auto operator/=(Vector3& left, float const right) noexcept -> Vector3& {
+	auto const memMask{ _mm_set_epi32(0, 1 << 31, 1 << 31, 1 << 31) };
+	auto const xmm0{ _mm_maskload_ps(left.GetData(), memMask) };
+	auto const xmm1{ _mm_broadcast_ss(&right) };
+	auto const xmm2{ _mm_div_ps(xmm0, xmm1) };
+	_mm_maskstore_ps(left.GetData(), memMask, xmm2);
+	return left;
+}
+
+
+template<>
+inline auto operator/=(Vector3& left, Vector3 const& right) noexcept -> Vector3& {
+	auto const memMask{ _mm_set_epi32(0, 1 << 31, 1 << 31, 1 << 31) };
+	auto const xmm0{ _mm_maskload_ps(left.GetData(), memMask) };
+	auto const xmm1{ _mm_maskload_ps(right.GetData(), memMask) };
+	auto const xmm2{ _mm_div_ps(xmm0, xmm1) };
+	_mm_maskstore_ps(left.GetData(), memMask, xmm2);
+	return left;
 }
 #endif
 
