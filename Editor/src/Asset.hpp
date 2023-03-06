bfe0e44b4eb32b70abab8b1ccf6641251e3d4e39
Levente Löffler <levente.loffler99@gmail.com>
2023-03-06 10:22:07 +0100

    Remove unused old asset pipeline functions

--- Editor/src/Asset.hpp
+++ Editor/src/Asset.hpp
@@ -3,10 +3,6 @@
 #include <Object.hpp>
 #include "ObjectFactoryManager.hpp"
 
-#include <memory>
-#include <filesystem>
-#include <span>
-
 namespace leopph::editor {
 struct AssetMetaInfo {
 	Object::Type type;
@@ -14,8 +10,6 @@ struct AssetMetaInfo {
 	int importPrecedence;
 };
 
-[[nodiscard]] auto LoadAsset(std::filesystem::path const& filePath) -> std::shared_ptr<Object>;
 [[nodiscard]] auto GenerateAssetMetaFileContents(Object const& asset, EditorObjectFactoryManager const& factoryManager) -> std::string;
 [[nodiscard]] auto ReadAssetMetaFileContents(std::string const& contents) -> AssetMetaInfo;
-auto AssignAssetMetaContents(Object& asset, std::span<u8 const> metaContentBytes) -> void;
 }
