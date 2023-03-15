067a3a3c1fa3098e572040b1fba7332e46b3b8a4
Levente Löffler <levente.loffler99@gmail.com>
2023-03-15 17:20:15 +0100

    Add srgb correction to skybox shader

--- RuntimeNative/src/shaders/SkyboxPS.hlsl
+++ RuntimeNative/src/shaders/SkyboxPS.hlsl
@@ -5,5 +5,5 @@ TextureCube gCubemap : register(t0);
 
 float4 main(const SkyboxVSOut vsOut) : SV_TARGET
 {
-    return float4(gCubemap.Sample(gSampler, vsOut.uv).rgb, 1);
+    return float4(pow(gCubemap.Sample(gSampler, vsOut.uv).rgb, 2.2), 1);
 }
