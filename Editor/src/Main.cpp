df1c84edccec283b0ee327e912ab12a99e46a753
Levente Löffler <levente.loffler99@gmail.com>
2023-03-19 11:02:11 +0100

    Add ImPlot dependency. Add frametime plot to performance window

--- Editor/src/Main.cpp
+++ Editor/src/Main.cpp
@@ -20,6 +20,7 @@
 #include <backends/imgui_impl_dx11.h>
 #include <misc/cpp/imgui_stdlib.h>
 #include <ImGuizmo.h>
+#include <implot.h>
 
 #include <nfd.h>
 
@@ -37,6 +38,7 @@
 #include <ranges>
 #include <string>
 #include <cwchar>
+#include <chrono>
 
 #include "EditorCamera.hpp"
 
@@ -790,8 +792,30 @@ auto DrawSceneOpenPrompt() {
 
 auto DrawPerformanceCounterWindow() {
 	if (ImGui::Begin("Performance")) {
-		ImGui::Text("%d %s", static_cast<int>(1.0f / get_frame_time()), "FPS");
-		ImGui::Text("%.2f %s", static_cast<double>(get_frame_time()), "ms");
+		std::chrono::duration<float, std::ratio<1>> const frameTimeSeconds{ get_frame_time() };
+		std::chrono::duration<float, std::milli> const frameTimeMillis{ frameTimeSeconds };
+
+		auto constexpr static MAX_DATA_POINT_COUNT{ 500 };
+		std::vector<float> static dataPoints;
+
+		if (dataPoints.size() == MAX_DATA_POINT_COUNT) {
+			dataPoints.erase(std::begin(dataPoints));
+		}
+
+		dataPoints.emplace_back(frameTimeMillis.count());
+
+		ImGui::Text("%d FPS", static_cast<int>(1.0f / frameTimeSeconds.count()));
+		ImGui::Text("%.2f ms", static_cast<double>(frameTimeMillis.count()));
+
+		if (ImPlot::BeginPlot("###frameTimeChart", ImVec2{ 0, 0 }, ImPlotFlags_NoInputs | ImPlotFlags_NoFrame)) {
+			ImPlot::SetupAxisLimits(ImAxis_Y1, 0.0, static_cast<double>(*std::ranges::max_element(dataPoints)), ImPlotCond_Always);
+			ImPlot::SetupAxisLimits(ImAxis_X1, 0, static_cast<double>(dataPoints.size()), ImPlotCond_Always);
+			ImPlot::SetupAxis(ImAxis_X1, nullptr, ImPlotAxisFlags_NoDecorations);
+			ImPlot::SetupAxis(ImAxis_Y1, nullptr, ImPlotAxisFlags_NoTickMarks);
+			ImPlot::SetupAxisFormat(ImAxis_Y1, "%.0f ms");
+			ImPlot::PlotLine("###frameTimeLine", dataPoints.data(), static_cast<int>(dataPoints.size()));
+			ImPlot::EndPlot();
+		}
 	}
 
 	ImGui::End();
@@ -819,6 +843,8 @@ auto WINAPI wWinMain([[maybe_unused]] _In_ HINSTANCE, [[maybe_unused]] _In_opt_ 
 		imGuiIo.IniFilename = iniFilePathStr.c_str();
 		imGuiIo.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
 
+		ImPlot::CreateContext();
+
 		ImGui::StyleColorsDark();
 
 		ImGui_ImplWin32_Init(leopph::gWindow.GetHandle());
@@ -840,15 +866,15 @@ auto WINAPI wWinMain([[maybe_unused]] _In_ HINSTANCE, [[maybe_unused]] _In_opt_ 
 			if (argc > 0) {
 				std::filesystem::path targetProjPath{ argv[0] };
 				targetProjPath = absolute(targetProjPath);
-				context.OpenProject(targetProjPath);
+					context.OpenProject(targetProjPath);
 			}
 
 			if (argc > 1) {
 				std::filesystem::path targetScenePath{ argv[1] };
 				targetScenePath = context.GetAssetDirectoryAbsolute() / targetScenePath;
-				if (auto const targetScene{ context.GetResources().TryGetAssetAt(targetScenePath) }) {
-					context.OpenScene(dynamic_cast<leopph::Scene&>(*targetScene));
-				}
+					if (auto const targetScene{ context.GetResources().TryGetAssetAt(targetScenePath) }) {
+						context.OpenScene(dynamic_cast<leopph::Scene&>(*targetScene));
+					}
 			}
 
 			LocalFree(argv);
@@ -930,6 +956,7 @@ auto WINAPI wWinMain([[maybe_unused]] _In_ HINSTANCE, [[maybe_unused]] _In_opt_ 
 
 		ImGui_ImplDX11_Shutdown();
 		ImGui_ImplWin32_Shutdown();
+		ImPlot::DestroyContext();
 		ImGui::DestroyContext();
 	}
 	catch (std::exception const& ex) {
