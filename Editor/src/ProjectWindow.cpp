1c9ce9f29cac797487a59b29bd97654bd43737b1
Levente Löffler <levente.loffler99@gmail.com>
2023-04-06 08:45:13 +0200

    Remove warning about sprintf in stb_image_write

--- Editor/src/ProjectWindow.cpp
+++ Editor/src/ProjectWindow.cpp
@@ -1,10 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+
 #include "ProjectWindow.hpp"
 
 #include <nfd.h>
 #include <stb_image.h>
-#define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <stb_image_write.h>
-
 #include <misc/cpp/imgui_stdlib.h>
 
 #include <functional>
