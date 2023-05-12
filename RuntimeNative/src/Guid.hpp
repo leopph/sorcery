e28b48900bb758d51b96dac33c6ca32948d8b890
Levente Löffler <levente.loffler99@gmail.com>
2023-05-12 06:39:35 +0200

    Guids are now default constructible and zero by default. Objects no longer generate a GUID for themselves on creation.

--- RuntimeNative/src/Guid.hpp
+++ RuntimeNative/src/Guid.hpp
@@ -4,20 +4,20 @@
 
 #include <string>
 
+
 namespace leopph {
-	class Guid {
-		u64 mData0{};
-		u64 mData1{};
+class Guid {
+  u64 mData0{ 0 };
+  u64 mData1{ 0 };
 
-		Guid() = default;
+public:
+  Guid() = default;
+  LEOPPHAPI Guid(u64 data0, u64 data1);
 
-	public:
-		LEOPPHAPI Guid(u64 data0, u64 data1);
+  [[nodiscard]] LEOPPHAPI static auto Generate() -> Guid;
+  [[nodiscard]] LEOPPHAPI static auto Parse(std::string_view str) -> Guid;
 
-		[[nodiscard]] LEOPPHAPI static auto Generate() -> Guid;
-		[[nodiscard]] LEOPPHAPI static auto Parse(std::string_view str) -> Guid;
-
-		[[nodiscard]] LEOPPHAPI auto ToString() const -> std::string;
-		[[nodiscard]] LEOPPHAPI auto operator==(Guid const& other) const -> bool;
-	};
+  [[nodiscard]] LEOPPHAPI auto ToString() const -> std::string;
+  [[nodiscard]] LEOPPHAPI auto operator==(Guid const& other) const -> bool;
+};
 }
--- RuntimeNative/src/Object.hpp
+++ RuntimeNative/src/Object.hpp
@@ -9,139 +9,142 @@
 #include <string>
 #include <vector>
 
+
 namespace leopph {
 class Object {
-	struct GuidObjectLess {
-		using is_transparent = void;
-		[[nodiscard]] auto operator()(Guid const& left, Guid const& right) const -> bool;
-		[[nodiscard]] auto operator()(Object const* left, Guid const& right) const -> bool;
-		[[nodiscard]] auto operator()(Guid const& left, Object const* right) const -> bool;
-		[[nodiscard]] auto operator()(Object const* left, Object const* right) const -> bool;
-	};
+  struct GuidObjectLess {
+    using is_transparent = void;
+    [[nodiscard]] auto operator()(Guid const& left, Guid const& right) const -> bool;
+    [[nodiscard]] auto operator()(Object const* left, Guid const& right) const -> bool;
+    [[nodiscard]] auto operator()(Guid const& left, Object const* right) const -> bool;
+    [[nodiscard]] auto operator()(Object const* left, Object const* right) const -> bool;
+  };
 
-	LEOPPHAPI static std::set<Object*, GuidObjectLess> sAllObjects;
 
-	Guid mGuid{ Guid::Generate() };
-	std::string mName{ "New Object" };
+  LEOPPHAPI static std::set<Object*, GuidObjectLess> sAllObjects;
+
+  Guid mGuid;
+  std::string mName{ "New Object" };
 
 public:
-	enum class Type {
-		Entity,
-		Transform,
-		Camera,
-		Behavior,
-		StaticMesh,
-		Light,
-		Material,
-		Mesh,
-		Scene,
-		Texture2D,
-		Cubemap,
-		Skybox
-	};
+  enum class Type {
+    Entity,
+    Transform,
+    Camera,
+    Behavior,
+    StaticMesh,
+    Light,
+    Material,
+    Mesh,
+    Scene,
+    Texture2D,
+    Cubemap,
+    Skybox
+  };
 
-	[[nodiscard]] LEOPPHAPI static auto FindObjectByGuid(Guid const& guid) -> Object*;
 
-	template<std::derived_from<Object> T>
-	[[nodiscard]] static auto FindObjectOfType() -> T*;
+  [[nodiscard]] LEOPPHAPI static auto FindObjectByGuid(Guid const& guid) -> Object*;
 
-	template<std::derived_from<Object> T>
-	static auto FindObjectsOfType(std::vector<T*>& out) -> std::vector<T*>&;
+  template<std::derived_from<Object> T>
+  [[nodiscard]] static auto FindObjectOfType() -> T*;
 
-	template<std::derived_from<Object> T>
-	[[nodiscard]] auto FindObjectsOfType() -> std::vector<T*>;
+  template<std::derived_from<Object> T>
+  static auto FindObjectsOfType(std::vector<T*>& out) -> std::vector<T*>&;
 
-	template<std::derived_from<Object> T>
-	static auto FindObjectsOfType(std::vector<Object*>& out) -> std::vector<Object*>&;
+  template<std::derived_from<Object> T>
+  [[nodiscard]] auto FindObjectsOfType() -> std::vector<T*>;
 
-	LEOPPHAPI Object();
-	Object(Object const& other) = delete;
-	Object(Object&& other) = delete;
+  template<std::derived_from<Object> T>
+  static auto FindObjectsOfType(std::vector<Object*>& out) -> std::vector<Object*>&;
 
-	LEOPPHAPI virtual ~Object();
+  LEOPPHAPI Object();
+  Object(Object const& other) = delete;
+  Object(Object&& other) = delete;
 
-	auto operator=(Object const& other) -> void = delete;
-	auto operator=(Object&& other) -> void = delete;
+  LEOPPHAPI virtual ~Object();
 
-	[[nodiscard]] LEOPPHAPI auto GetGuid() const -> Guid const&;
-	LEOPPHAPI auto SetGuid(Guid const& guid) -> void;
+  auto operator=(Object const& other) -> void = delete;
+  auto operator=(Object&& other) -> void = delete;
 
-	[[nodiscard]] LEOPPHAPI auto GetName() const noexcept -> std::string_view;
-	LEOPPHAPI auto SetName(std::string name) noexcept -> void;
+  [[nodiscard]] LEOPPHAPI auto GetGuid() const -> Guid const&;
+  LEOPPHAPI auto SetGuid(Guid const& guid) -> void;
 
-	[[nodiscard]] virtual auto GetSerializationType() const -> Type = 0;
+  [[nodiscard]] LEOPPHAPI auto GetName() const noexcept -> std::string_view;
+  LEOPPHAPI auto SetName(std::string name) noexcept -> void;
+
+  [[nodiscard]] virtual auto GetSerializationType() const -> Type = 0;
 };
 
 
 class ObjectInstantiator {
 public:
-	ObjectInstantiator() = default;
-	ObjectInstantiator(ObjectInstantiator const& other) = default;
-	ObjectInstantiator(ObjectInstantiator&& other) noexcept = default;
+  ObjectInstantiator() = default;
+  ObjectInstantiator(ObjectInstantiator const& other) = default;
+  ObjectInstantiator(ObjectInstantiator&& other) noexcept = default;
 
-	virtual ~ObjectInstantiator() = default;
+  virtual ~ObjectInstantiator() = default;
 
-	auto operator=(ObjectInstantiator const& other) -> ObjectInstantiator& = default;
-	auto operator=(ObjectInstantiator&& other) noexcept -> ObjectInstantiator& = default;
+  auto operator=(ObjectInstantiator const& other) -> ObjectInstantiator& = default;
+  auto operator=(ObjectInstantiator&& other) noexcept -> ObjectInstantiator& = default;
 
-	[[nodiscard]] virtual auto Instantiate() -> Object* = 0;
+  [[nodiscard]] virtual auto Instantiate() -> Object* = 0;
 };
 
 
 class ObjectInstantiatorManager {
 public:
-	ObjectInstantiatorManager() = default;
-	ObjectInstantiatorManager(ObjectInstantiatorManager const& other) = default;
-	ObjectInstantiatorManager(ObjectInstantiatorManager&& other) noexcept = default;
+  ObjectInstantiatorManager() = default;
+  ObjectInstantiatorManager(ObjectInstantiatorManager const& other) = default;
+  ObjectInstantiatorManager(ObjectInstantiatorManager&& other) noexcept = default;
 
-	virtual ~ObjectInstantiatorManager() = default;
+  virtual ~ObjectInstantiatorManager() = default;
 
-	auto operator=(ObjectInstantiatorManager const& other) -> ObjectInstantiatorManager& = default;
-	auto operator=(ObjectInstantiatorManager&& other) noexcept -> ObjectInstantiatorManager& = default;
+  auto operator=(ObjectInstantiatorManager const& other) -> ObjectInstantiatorManager& = default;
+  auto operator=(ObjectInstantiatorManager&& other) noexcept -> ObjectInstantiatorManager& = default;
 
-	[[nodiscard]] virtual auto GetFor(Object::Type type) const -> ObjectInstantiator& = 0;
+  [[nodiscard]] virtual auto GetFor(Object::Type type) const -> ObjectInstantiator& = 0;
 };
 
 
 template<std::derived_from<Object> T>
 auto Object::FindObjectOfType() -> T* {
-	for (auto const obj : sAllObjects) {
-		if (auto const castObj{ dynamic_cast<T*>(obj) }; castObj) {
-			return castObj;
-		}
-	}
-	return nullptr;
+  for (auto const obj : sAllObjects) {
+    if (auto const castObj{ dynamic_cast<T*>(obj) }; castObj) {
+      return castObj;
+    }
+  }
+  return nullptr;
 }
 
 
 template<std::derived_from<Object> T>
 auto Object::FindObjectsOfType(std::vector<T*>& out) -> std::vector<T*>& {
-	out.clear();
-	for (auto const obj : sAllObjects) {
-		if (auto const castObj{ dynamic_cast<T*>(obj) }; castObj) {
-			out.emplace_back(castObj);
-		}
-	}
-	return out;
+  out.clear();
+  for (auto const obj : sAllObjects) {
+    if (auto const castObj{ dynamic_cast<T*>(obj) }; castObj) {
+      out.emplace_back(castObj);
+    }
+  }
+  return out;
 }
 
 
 template<std::derived_from<Object> T>
 auto Object::FindObjectsOfType() -> std::vector<T*> {
-	std::vector<T*> ret;
-	FindObjectsOfType<T>(ret);
-	return ret;
+  std::vector<T*> ret;
+  FindObjectsOfType<T>(ret);
+  return ret;
 }
 
 
 template<std::derived_from<Object> T>
 auto Object::FindObjectsOfType(std::vector<Object*>& out) -> std::vector<Object*>& {
-	out.clear();
-	for (auto const obj : sAllObjects) {
-		if (dynamic_cast<T*>(obj)) {
-			out.emplace_back(obj);
-		}
-	}
-	return out;
+  out.clear();
+  for (auto const obj : sAllObjects) {
+    if (dynamic_cast<T*>(obj)) {
+      out.emplace_back(obj);
+    }
+  }
+  return out;
 }
 }
