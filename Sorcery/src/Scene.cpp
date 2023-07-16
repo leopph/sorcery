390a0db13c25b98e9cd0a2e9a221bf968c75540c
Levente Löffler <levente.loffler99@gmail.com>
2023-07-16 08:53:52 +0200

    Add reflection-based scene serialization

--- Sorcery/src/Scene.cpp
+++ Sorcery/src/Scene.cpp
@@ -9,6 +9,201 @@ RTTR_REGISTRATION {
 
 
 namespace sorcery {
+namespace {
+auto ReflectionSerializeToYAML(rttr::variant const& variant, std::unordered_map<void const*, int> const& ptrFixUp) -> YAML::Node {
+  auto const objType{ variant.get_type() };
+  auto const isWrapper{ objType.is_wrapper() };
+  auto const wrappedType{ objType.get_wrapped_type() };
+  auto const rawWrappedType{
+    isWrapper
+      ? wrappedType.get_raw_type()
+      : objType.get_raw_type()
+  };
+
+  YAML::Node ret;
+
+  if (rawWrappedType.is_arithmetic()) {
+    if (rawWrappedType == rttr::type::get<char>()) {
+      ret = isWrapper
+              ? variant.get_wrapped_value<char>()
+              : variant.get_value<char>();
+    } else if (rawWrappedType == rttr::type::get<signed char>()) {
+      ret = isWrapper
+              ? variant.get_wrapped_value<signed char>()
+              : variant.get_value<signed char>();
+    } else if (rawWrappedType == rttr::type::get<unsigned char>()) {
+      ret = isWrapper
+              ? variant.get_wrapped_value<unsigned char>()
+              : variant.get_value<unsigned char>();
+    } else if (rawWrappedType == rttr::type::get<short>()) {
+      ret = isWrapper
+              ? variant.get_wrapped_value<short>()
+              : variant.get_value<short>();
+    } else if (rawWrappedType == rttr::type::get<unsigned short>()) {
+      ret = isWrapper
+              ? variant.get_wrapped_value<unsigned short>()
+              : variant.get_value<unsigned short>();
+    } else if (rawWrappedType == rttr::type::get<int>()) {
+      ret = isWrapper
+              ? variant.get_wrapped_value<int>()
+              : variant.get_value<int>();
+    } else if (rawWrappedType == rttr::type::get<unsigned>()) {
+      ret = isWrapper
+              ? variant.get_wrapped_value<unsigned>()
+              : variant.get_value<unsigned>();
+    } else if (rawWrappedType == rttr::type::get<long>()) {
+      ret = isWrapper
+              ? variant.get_wrapped_value<long>()
+              : variant.get_value<long>();
+    } else if (rawWrappedType == rttr::type::get<unsigned long>()) {
+      ret = isWrapper
+              ? variant.get_wrapped_value<unsigned long>()
+              : variant.get_value<unsigned long>();
+    } else if (rawWrappedType == rttr::type::get<long long>()) {
+      ret = isWrapper
+              ? variant.get_wrapped_value<long long>()
+              : variant.get_value<long long>();
+    } else if (rawWrappedType == rttr::type::get<unsigned long long>()) {
+      ret = isWrapper
+              ? variant.get_wrapped_value<unsigned long long>()
+              : variant.get_value<unsigned long long>();
+    } else if (rawWrappedType == rttr::type::get<float>()) {
+      ret = isWrapper
+              ? variant.get_wrapped_value<float>()
+              : variant.get_value<float>();
+    } else if (rawWrappedType == rttr::type::get<double>()) {
+      ret = isWrapper
+              ? variant.get_wrapped_value<double>()
+              : variant.get_value<double>();
+    } else if (rawWrappedType == rttr::type::get<long double>()) {
+      ret = isWrapper
+              ? variant.get_wrapped_value<long double>()
+              : variant.get_value<long double>();
+    }
+  } else if (rawWrappedType.is_sequential_container()) {
+    for (auto const& elem : variant.create_sequential_view()) {
+      ret.push_back(ReflectionSerializeToYAML(elem, ptrFixUp));
+    }
+  } else if (objType.is_pointer() && rawWrappedType.is_derived_from(rttr::type::get<SceneElement>())) {
+    auto const ptr{
+      isWrapper
+        ? variant.get_wrapped_value<void const*>()
+        : variant.get_value<void const*>()
+    };
+
+    if (auto const it{ ptrFixUp.find(ptr) }; it != std::end(ptrFixUp)) {
+      ret = it->second;
+    } else {
+      ret = 0;
+    }
+  } else if (rawWrappedType.is_class()) {
+    for (auto const& prop : objType.get_properties()) {
+      ret[prop.get_name().to_string()] = ReflectionSerializeToYAML(prop.get_value(variant), ptrFixUp);
+    }
+  }
+
+  return ret;
+}
+
+
+template<typename T>
+auto ReflectionSerializeToYAML(T const& obj, std::unordered_map<void const*, int> const& ptrFixUp) -> YAML::Node {
+  auto const objType{ rttr::type::get<T>() };
+
+  YAML::Node ret;
+  ret["type"] = objType.get_name().to_string();
+
+  for (auto const& prop : objType.get_properties()) {
+    ret["properties"][prop.get_name().to_string()] = ReflectionSerializeToYAML(prop.get_value(obj), ptrFixUp);
+  }
+
+  return ret;
+}
+
+
+auto ReflectioNDeserializeFromYAML(YAML::Node const& objNode, std::unordered_map<int, std::shared_ptr<SceneElement>> const& ptrFixUp, rttr::variant& variant) -> void {
+  auto const objType{ variant.get_type() };
+  auto const isWrapper{ objType.is_wrapper() };
+  auto const wrappedType{ objType.get_wrapped_type() };
+  auto const rawWrappedType{
+    isWrapper
+      ? wrappedType.get_raw_type()
+      : objType.get_raw_type()
+  };
+
+  if (rawWrappedType.is_arithmetic()) {
+    if (rawWrappedType == rttr::type::get<char>()) {
+      variant = objNode.as<char>();
+    } else if (rawWrappedType == rttr::type::get<signed char>()) {
+      variant = objNode.as<signed char>();
+    } else if (rawWrappedType == rttr::type::get<unsigned char>()) {
+      variant = objNode.as<unsigned char>();
+    } else if (rawWrappedType == rttr::type::get<short>()) {
+      variant = objNode.as<short>();
+    } else if (rawWrappedType == rttr::type::get<unsigned short>()) {
+      variant = objNode.as<unsigned short>();
+    } else if (rawWrappedType == rttr::type::get<int>()) {
+      variant = objNode.as<int>();
+    } else if (rawWrappedType == rttr::type::get<unsigned>()) {
+      variant = objNode.as<unsigned>();
+    } else if (rawWrappedType == rttr::type::get<long>()) {
+      variant = objNode.as<long>();
+    } else if (rawWrappedType == rttr::type::get<unsigned long>()) {
+      variant = objNode.as<unsigned long>();
+    } else if (rawWrappedType == rttr::type::get<long long>()) {
+      variant = objNode.as<long long>();
+    } else if (rawWrappedType == rttr::type::get<unsigned long long>()) {
+      variant = objNode.as<unsigned long long>();
+    } else if (rawWrappedType == rttr::type::get<float>()) {
+      variant = objNode.as<float>();
+    } else if (rawWrappedType == rttr::type::get<double>()) {
+      variant = objNode.as<double>();
+    } else if (rawWrappedType == rttr::type::get<long double>()) {
+      variant = objNode.as<long double>();
+    }
+  } else if (rawWrappedType.is_sequential_container()) {
+    auto sequence{ variant.create_sequential_view() };
+
+    for (auto const& elemNode : objNode) {
+      rttr::variant elem;
+      ReflectioNDeserializeFromYAML(elemNode, ptrFixUp, elem);
+      sequence.insert(sequence.end(), elem);
+    }
+  } else if (objType.is_pointer() && rawWrappedType.is_derived_from(rttr::type::get<SceneElement>())) {
+    if (auto const ptrId{ objNode.as<int>() }; ptrId != 0) {
+      if (auto const it{ ptrFixUp.find(ptrId) }; it != std::end(ptrFixUp)) {
+        variant = it->second.get();
+      } else {
+        variant = nullptr;
+      }
+    } else {
+      variant = nullptr;
+    }
+  } else if (rawWrappedType.is_class()) {
+    for (auto const& prop : objType.get_properties()) {
+      rttr::variant propValue;
+      ReflectioNDeserializeFromYAML(objNode[prop.get_name().to_string()], ptrFixUp, propValue);
+      std::ignore = prop.set_value(variant, propValue);
+    }
+  }
+}
+
+
+auto ReflectionDeserializeFromYAML(YAML::Node const& objNode, std::unordered_map<int, std::shared_ptr<SceneElement>> const& ptrFixUp, Object& obj) -> void {
+  auto const objType{ rttr::type::get(obj) };
+
+  auto const& propertiesNode{ objNode["properties"] };
+
+  for (auto it{ propertiesNode.begin() }; it != propertiesNode.end(); ++it) {
+    auto const prop{ objType.get_property(it->first.as<std::string>()) };
+    auto propValue{ prop.get_value(obj) };
+    ReflectioNDeserializeFromYAML(it->second, ptrFixUp, propValue);
+    std::ignore = prop.set_value(obj, propValue);
+  }
+}
+}
+
+
 Scene* Scene::sActiveScene{ nullptr };
 std::vector<Scene*> Scene::sAllScenes;
 Object::Type const Scene::SerializationType{ Type::Scene };
@@ -79,26 +274,25 @@ auto Scene::Save() -> void {
   mYamlData.reset();
   mYamlData["version"] = 1;
 
-  for (auto const& entity : mEntities) {
-    YAML::Node entityDataNode;
-    entity->Serialize(entityDataNode);
+  static std::unordered_map<void const*, int> ptrFixUp;
 
-    YAML::Node entityNode;
-    entityNode["data"] = entityDataNode;
+  for (auto const& entity : mEntities) {
+    ptrFixUp[entity.get()] = static_cast<int>(std::ssize(ptrFixUp) + 1);
 
     for (auto const& component : entity->mComponents) {
-      YAML::Node componentDataNode;
-      component->Serialize(componentDataNode);
-
-      YAML::Node componentNode;
-      componentNode["type"] = static_cast<int>(component->GetSerializationType());
-      componentNode["data"] = componentDataNode;
-
-      entityNode["components"].push_back(componentNode);
+      ptrFixUp[component.get()] = static_cast<int>(std::ssize(ptrFixUp) + 1);
     }
-
-    mYamlData["entities"].push_back(entityNode);
   }
+
+  for (auto const& entity : mEntities) {
+    mYamlData["objects"].push_back(ReflectionSerializeToYAML(*entity, ptrFixUp));
+
+    for (auto const& component : entity->mComponents) {
+      mYamlData["objects"].push_back(ReflectionSerializeToYAML(*component, ptrFixUp));
+    }
+  }
+
+  ptrFixUp.clear();
 }
 
 
@@ -106,21 +300,23 @@ auto Scene::Load(ObjectInstantiatorManager const& manager) -> void {
   sActiveScene = this;
   mEntities.clear();
 
-  if (auto const version{ mYamlData["version"] }; version && version.IsScalar() && version.as<int>(1) == 1) {
-    for (auto const entityNode : mYamlData["entities"]) {
-      auto const& entity{ mEntities.emplace_back(new Entity{}) };
-      entity->Deserialize(entityNode["data"]);
-
-      for (auto const componentNode : entityNode["components"]) {
-        if (auto const component{ dynamic_cast<Component*>(manager.GetFor(static_cast<Type>(componentNode["type"].as<int>())).Instantiate()) }) {
-          entity->AddComponent(std::shared_ptr<Component>{ component });
-          component->Deserialize(componentNode["data"]);
-        }
-      }
-    }
-  } else {
+  if (auto const version{ mYamlData["version"] }; !version || !version.IsScalar() || version.as<int>(1) != 1) {
     throw std::runtime_error{ std::format("Couldn't load scene \"{}\" because its version number is unsupported.", GetName()) };
   }
+
+  static std::unordered_map<int, std::shared_ptr<SceneElement>> ptrFixUp;
+
+  for (auto const objectNode : mYamlData["objects"]) {
+    auto const typeNode{ objectNode["type"] };
+    auto const type{ rttr::type::get(typeNode.as<std::string>()) };
+    ptrFixUp[static_cast<int>(std::ssize(ptrFixUp)) + 1] = type.create().get_value<std::shared_ptr<SceneElement>>();
+  }
+
+  for (auto const& [fileId, obj] : ptrFixUp) {
+    ReflectionDeserializeFromYAML(mYamlData["objects"][fileId - 1], ptrFixUp, *obj);
+  }
+
+  ptrFixUp.clear();
 }
 
 
