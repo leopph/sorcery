eb654cfc6c58d62defaf3f1c8a91f9f8576af702
Levente Löffler <levente.loffler99@gmail.com>
2023-07-21 08:39:36 +0200

    Add reflection-based serialization to Serialization.hpp

--- Sorcery/src/Serialization.cpp
+++ Sorcery/src/Serialization.cpp
@@ -1,6 +1,31 @@
 #include "Serialization.hpp"
 
 
+namespace YAML {
+auto convert<sorcery::Quaternion>::encode(sorcery::Quaternion const& q) -> Node {
+  Node node;
+  node.SetStyle(EmitterStyle::Flow);
+  node.push_back(q.w);
+  node.push_back(q.x);
+  node.push_back(q.y);
+  node.push_back(q.z);
+  return node;
+}
+
+
+auto convert<sorcery::Quaternion>::decode(Node const& node, sorcery::Quaternion& q) -> bool {
+  if (!node.IsSequence() || node.size() != 4) {
+    return false;
+  }
+  q.w = node[0].as<sorcery::f32>();
+  q.x = node[1].as<sorcery::f32>();
+  q.y = node[2].as<sorcery::f32>();
+  q.z = node[3].as<sorcery::f32>();
+  return true;
+}
+}
+
+
 namespace sorcery {
 auto BinarySerializer<std::string>::Serialize(std::string_view const str, std::endian const endianness, std::vector<std::uint8_t>& out) -> void {
   BinarySerializer<std::size_t>::Serialize(std::size(str), endianness, out);
@@ -114,5 +139,131 @@ auto BinarySerializer<Mesh::Data>::Deserialize(std::span<std::uint8_t const> byt
   bytes = BinarySerializer<std::vector<std::uint32_t>>::Deserialize(bytes, endianness, out.indices);
   bytes = BinarySerializer<std::vector<Mesh::SubMeshData>>::Deserialize(bytes, endianness, out.subMeshes);
   return bytes;
+}
+
+
+namespace detail {
+auto ReflectionSerializeToYAML(rttr::variant const& variant, std::function<YAML::Node(rttr::variant const&)> const& extensionFunc) -> YAML::Node {
+  auto const objType{ variant.get_type() };
+  auto const isWrapper{ objType.is_wrapper() };
+  auto const wrappedType{ objType.get_wrapped_type() };
+  auto const rawWrappedType{
+    isWrapper
+      ? wrappedType.get_raw_type()
+      : objType.get_raw_type()
+  };
+
+  YAML::Node ret;
+
+  if (rawWrappedType.is_arithmetic()) {
+    if (rawWrappedType == rttr::type::get<char>()) {
+      ret = isWrapper
+              ? variant.get_wrapped_value<char>()
+              : variant.get_value<char>();
+    } else if (rawWrappedType == rttr::type::get<signed char>()) {
+      ret = isWrapper
+              ? variant.get_wrapped_value<signed char>()
+              : variant.get_value<signed char>();
+    } else if (rawWrappedType == rttr::type::get<unsigned char>()) {
+      ret = isWrapper
+              ? variant.get_wrapped_value<unsigned char>()
+              : variant.get_value<unsigned char>();
+    } else if (rawWrappedType == rttr::type::get<short>()) {
+      ret = isWrapper
+              ? variant.get_wrapped_value<short>()
+              : variant.get_value<short>();
+    } else if (rawWrappedType == rttr::type::get<unsigned short>()) {
+      ret = isWrapper
+              ? variant.get_wrapped_value<unsigned short>()
+              : variant.get_value<unsigned short>();
+    } else if (rawWrappedType == rttr::type::get<int>()) {
+      ret = isWrapper
+              ? variant.get_wrapped_value<int>()
+              : variant.get_value<int>();
+    } else if (rawWrappedType == rttr::type::get<unsigned>()) {
+      ret = isWrapper
+              ? variant.get_wrapped_value<unsigned>()
+              : variant.get_value<unsigned>();
+    } else if (rawWrappedType == rttr::type::get<long>()) {
+      ret = isWrapper
+              ? variant.get_wrapped_value<long>()
+              : variant.get_value<long>();
+    } else if (rawWrappedType == rttr::type::get<unsigned long>()) {
+      ret = isWrapper
+              ? variant.get_wrapped_value<unsigned long>()
+              : variant.get_value<unsigned long>();
+    } else if (rawWrappedType == rttr::type::get<long long>()) {
+      ret = isWrapper
+              ? variant.get_wrapped_value<long long>()
+              : variant.get_value<long long>();
+    } else if (rawWrappedType == rttr::type::get<unsigned long long>()) {
+      ret = isWrapper
+              ? variant.get_wrapped_value<unsigned long long>()
+              : variant.get_value<unsigned long long>();
+    } else if (rawWrappedType == rttr::type::get<float>()) {
+      ret = isWrapper
+              ? variant.get_wrapped_value<float>()
+              : variant.get_value<float>();
+    } else if (rawWrappedType == rttr::type::get<double>()) {
+      ret = isWrapper
+              ? variant.get_wrapped_value<double>()
+              : variant.get_value<double>();
+    } else if (rawWrappedType == rttr::type::get<long double>()) {
+      ret = isWrapper
+              ? variant.get_wrapped_value<long double>()
+              : variant.get_value<long double>();
+    }
+  } else if (extensionFunc) {
+    ret = extensionFunc(variant);
+  }
+
+  return ret;
+}
+
+
+auto ReflectionDeserializeFromYAML(YAML::Node const& objNode, rttr::variant& variant, std::function<void(YAML::Node const&, rttr::variant&)> const& extensionFunc) -> void {
+  auto const objType{ variant.get_type() };
+  auto const isWrapper{ objType.is_wrapper() };
+  auto const wrappedType{ objType.get_wrapped_type() };
+  auto const rawWrappedType{
+    isWrapper
+      ? wrappedType.get_raw_type()
+      : objType.get_raw_type()
+  };
+
+  if (rawWrappedType.is_arithmetic()) {
+    if (rawWrappedType == rttr::type::get<char>()) {
+      variant = objNode.as<char>();
+    } else if (rawWrappedType == rttr::type::get<signed char>()) {
+      variant = objNode.as<signed char>();
+    } else if (rawWrappedType == rttr::type::get<unsigned char>()) {
+      variant = objNode.as<unsigned char>();
+    } else if (rawWrappedType == rttr::type::get<short>()) {
+      variant = objNode.as<short>();
+    } else if (rawWrappedType == rttr::type::get<unsigned short>()) {
+      variant = objNode.as<unsigned short>();
+    } else if (rawWrappedType == rttr::type::get<int>()) {
+      variant = objNode.as<int>();
+    } else if (rawWrappedType == rttr::type::get<unsigned>()) {
+      variant = objNode.as<unsigned>();
+    } else if (rawWrappedType == rttr::type::get<long>()) {
+      variant = objNode.as<long>();
+    } else if (rawWrappedType == rttr::type::get<unsigned long>()) {
+      variant = objNode.as<unsigned long>();
+    } else if (rawWrappedType == rttr::type::get<long long>()) {
+      variant = objNode.as<long long>();
+    } else if (rawWrappedType == rttr::type::get<unsigned long long>()) {
+      variant = objNode.as<unsigned long long>();
+    } else if (rawWrappedType == rttr::type::get<float>()) {
+      variant = objNode.as<float>();
+    } else if (rawWrappedType == rttr::type::get<double>()) {
+      variant = objNode.as<double>();
+    } else if (rawWrappedType == rttr::type::get<long double>()) {
+      variant = objNode.as<long double>();
+    }
+  } else if (extensionFunc) {
+    extensionFunc(objNode, variant);
+  }
+}
 }
 }
--- Sorcery/src/Serialization.hpp
+++ Sorcery/src/Serialization.hpp
@@ -16,6 +16,7 @@
 #include "Image.hpp"
 #include "Math.hpp"
 #include "Resources/Mesh.hpp"
+#include "Reflection.hpp"
 
 // yaml-cpp incorrectly uses dllexport specifiers so we silence their warnings
 #pragma warning (push)
@@ -27,51 +28,15 @@
 namespace YAML {
 template<typename T, std::size_t N>
 struct convert<sorcery::Vector<T, N>> {
-  static auto encode(sorcery::Vector<T, N> const& v) -> Node {
-    Node node;
-    node.SetStyle(EmitterStyle::Flow);
-    for (std::size_t i = 0; i < N; i++) {
-      node.push_back(v[i]);
-    }
-    return node;
-  }
-
-
-  static auto decode(Node const& node, sorcery::Vector<T, N>& v) -> bool {
-    if (!node.IsSequence() || node.size() != N) {
-      return false;
-    }
-    for (std::size_t i = 0; i < N; i++) {
-      v[i] = node[i].as<T>();
-    }
-    return true;
-  }
+  static auto encode(sorcery::Vector<T, N> const& v) -> Node;
+  static auto decode(Node const& node, sorcery::Vector<T, N>& v) -> bool;
 };
 
 
 template<>
 struct convert<sorcery::Quaternion> {
-  static auto encode(sorcery::Quaternion const& q) -> Node {
-    Node node;
-    node.SetStyle(EmitterStyle::Flow);
-    node.push_back(q.w);
-    node.push_back(q.x);
-    node.push_back(q.y);
-    node.push_back(q.z);
-    return node;
-  }
-
-
-  static auto decode(Node const& node, sorcery::Quaternion& q) -> bool {
-    if (!node.IsSequence() || node.size() != 4) {
-      return false;
-    }
-    q.w = node[0].as<sorcery::f32>();
-    q.x = node[1].as<sorcery::f32>();
-    q.y = node[2].as<sorcery::f32>();
-    q.z = node[3].as<sorcery::f32>();
-    return true;
-  }
+  LEOPPHAPI static auto encode(sorcery::Quaternion const& q) -> Node;
+  LEOPPHAPI static auto decode(Node const& node, sorcery::Quaternion& q) -> bool;
 };
 }
 
@@ -83,89 +48,22 @@ struct BinarySerializer;
 
 template<typename T> requires (std::is_scalar_v<T>)
 struct BinarySerializer<T> {
-  static auto Serialize(T const scalar, [[maybe_unused]] std::endian const endianness, std::vector<std::uint8_t>& out) -> void {
-    if constexpr (sizeof(T) == 1) {
-      out.emplace_back(*reinterpret_cast<std::uint8_t const*>(&scalar));
-    } else {
-      auto const* const begin = reinterpret_cast<std::uint8_t const*>(&scalar);
-      auto const sz = sizeof(T);
-      auto const inserter = std::back_inserter(out);
-
-      if (endianness == std::endian::native) {
-        std::copy_n(begin, sz, inserter);
-        return;
-      }
-
-      std::copy_n(std::reverse_iterator{ begin + sz }, sz, inserter);
-    }
-  }
-
-
-  static auto Deserialize(std::span<std::uint8_t const> bytes, [[maybe_unused]] std::endian const endianness, T& out) -> std::span<std::uint8_t const> {
-    if (bytes.size() < sizeof(T)) {
-      throw std::runtime_error{ std::format("Cannot deserialize scalar of size {} because the source byte array is only of length {}.", sizeof(T), bytes.size()) };
-    }
-
-    if constexpr (sizeof(T) == 1) {
-      out = *reinterpret_cast<T const*>(bytes.data());
-    } else {
-      if (endianness == std::endian::native) {
-        out = *reinterpret_cast<T const*>(bytes.data());
-      } else {
-        auto static constexpr typeSz = sizeof(T);
-        std::array<std::uint8_t, typeSz> tmpBytes{};
-        std::copy_n(std::reverse_iterator{ bytes.data() + typeSz }, typeSz, tmpBytes.data());
-        out = *reinterpret_cast<T const*>(tmpBytes.data());
-      }
-    }
-
-    return bytes.subspan(sizeof(T));
-  }
+  static auto Serialize(T const scalar, [[maybe_unused]] std::endian const endianness, std::vector<std::uint8_t>& out) -> void;
+  static auto Deserialize(std::span<std::uint8_t const> bytes, [[maybe_unused]] std::endian const endianness, T& out) -> std::span<std::uint8_t const>;
 };
 
 
 template<typename T>
 struct BinarySerializer<std::vector<T>> {
-  static auto Serialize(std::span<T const> const arr, std::endian const endianness, std::vector<std::uint8_t>& out) -> void {
-    BinarySerializer<std::uint64_t>::Serialize(std::size(arr), endianness, out);
-
-    for (T const& elem : arr) {
-      BinarySerializer<T>::Serialize(elem, endianness, out);
-    }
-  }
-
-
-  static auto Deserialize(std::span<std::uint8_t const> bytes, std::endian const endianness, std::vector<T>& out) -> std::span<std::uint8_t const> {
-    std::uint64_t arrLength;
-    bytes = BinarySerializer<std::uint64_t>::Deserialize(bytes, endianness, arrLength);
-
-    out.resize(arrLength);
-
-    for (std::uint64_t i = 0; i < arrLength; i++) {
-      bytes = BinarySerializer<T>::Deserialize(bytes, endianness, out[i]);
-    }
-
-    return bytes;
-  }
+  static auto Serialize(std::span<T const> const arr, std::endian const endianness, std::vector<std::uint8_t>& out) -> void;
+  static auto Deserialize(std::span<std::uint8_t const> bytes, std::endian const endianness, std::vector<T>& out) -> std::span<std::uint8_t const>;
 };
 
 
 template<typename T, int N>
 struct BinarySerializer<Vector<T, N>> {
-  static auto Serialize(Vector<T, N> const& vector, std::endian const endianness, std::vector<std::uint8_t>& out) -> void {
-    for (int i = 0; i < N; i++) {
-      BinarySerializer<T>::Serialize(vector[i], endianness, out);
-    }
-  }
-
-
-  static auto Deserialize(std::span<std::uint8_t const> bytes, std::endian const endianness, Vector<T, N>& out) -> std::span<std::uint8_t const> {
-    for (int i = 0; i < N; i++) {
-      bytes = BinarySerializer<T>::Deserialize(bytes, endianness, out[i]);
-    }
-
-    return bytes;
-  }
+  static auto Serialize(Vector<T, N> const& vector, std::endian const endianness, std::vector<std::uint8_t>& out) -> void;
+  static auto Deserialize(std::span<std::uint8_t const> bytes, std::endian const endianness, Vector<T, N>& out) -> std::span<std::uint8_t const>;
 };
 
 
@@ -202,4 +100,170 @@ struct BinarySerializer<Mesh::Data> {
   LEOPPHAPI static auto Serialize(Mesh::Data const& meshData, std::endian endianness, std::vector<std::uint8_t>& out) -> void;
   LEOPPHAPI static auto Deserialize(std::span<std::uint8_t const> bytes, std::endian endianness, Mesh::Data& out) -> std::span<std::uint8_t const>;
 };
+
+
+template<typename T>
+[[nodiscard]] auto ReflectionSerializeToYAML(T const& obj, std::function<YAML::Node(rttr::variant const&)> const& extensionFunc = {}) -> YAML::Node;
+
+template<typename T>
+auto ReflectionDeserializeFromYAML(YAML::Node const& objNode, T& obj, std::function<void(YAML::Node const&, rttr::variant&)> const& extensionFunc = {}) -> void;
+}
+
+
+namespace YAML {
+template<typename T, std::size_t N>
+auto convert<sorcery::Vector<T, N>>::encode(sorcery::Vector<T, N> const& v) -> Node {
+  Node node;
+  node.SetStyle(EmitterStyle::Flow);
+  for (std::size_t i = 0; i < N; i++) {
+    node.push_back(v[i]);
+  }
+  return node;
+}
+
+
+template<typename T, std::size_t N>
+auto convert<sorcery::Vector<T, N>>::decode(Node const& node, sorcery::Vector<T, N>& v) -> bool {
+  if (!node.IsSequence() || node.size() != N) {
+    return false;
+  }
+  for (std::size_t i = 0; i < N; i++) {
+    v[i] = node[i].as<T>();
+  }
+  return true;
+}
+}
+
+
+namespace sorcery {
+template<typename T> requires (std::is_scalar_v<T>)
+auto BinarySerializer<T>::Serialize(T const scalar, std::endian const endianness, std::vector<std::uint8_t>& out) -> void {
+  if constexpr (sizeof(T) == 1) {
+    out.emplace_back(*reinterpret_cast<std::uint8_t const*>(&scalar));
+  } else {
+    auto const* const begin = reinterpret_cast<std::uint8_t const*>(&scalar);
+    auto const sz = sizeof(T);
+    auto const inserter = std::back_inserter(out);
+
+    if (endianness == std::endian::native) {
+      std::copy_n(begin, sz, inserter);
+      return;
+    }
+
+    std::copy_n(std::reverse_iterator{ begin + sz }, sz, inserter);
+  }
+}
+
+
+template<typename T> requires (std::is_scalar_v<T>)
+auto BinarySerializer<T>::Deserialize(std::span<std::uint8_t const> bytes, std::endian const endianness, T& out) -> std::span<std::uint8_t const> {
+  if (bytes.size() < sizeof(T)) {
+    throw std::runtime_error{ std::format("Cannot deserialize scalar of size {} because the source byte array is only of length {}.", sizeof(T), bytes.size()) };
+  }
+
+  if constexpr (sizeof(T) == 1) {
+    out = *reinterpret_cast<T const*>(bytes.data());
+  } else {
+    if (endianness == std::endian::native) {
+      out = *reinterpret_cast<T const*>(bytes.data());
+    } else {
+      auto static constexpr typeSz = sizeof(T);
+      std::array<std::uint8_t, typeSz> tmpBytes{};
+      std::copy_n(std::reverse_iterator{ bytes.data() + typeSz }, typeSz, tmpBytes.data());
+      out = *reinterpret_cast<T const*>(tmpBytes.data());
+    }
+  }
+
+  return bytes.subspan(sizeof(T));
+}
+
+
+template<typename T>
+auto BinarySerializer<std::vector<T>>::Serialize(std::span<T const> const arr, std::endian const endianness, std::vector<std::uint8_t>& out) -> void {
+  BinarySerializer<std::uint64_t>::Serialize(std::size(arr), endianness, out);
+
+  for (T const& elem : arr) {
+    BinarySerializer<T>::Serialize(elem, endianness, out);
+  }
+}
+
+
+template<typename T>
+auto BinarySerializer<std::vector<T>>::Deserialize(std::span<std::uint8_t const> bytes, std::endian const endianness, std::vector<T>& out) -> std::span<std::uint8_t const> {
+  std::uint64_t arrLength;
+  bytes = BinarySerializer<std::uint64_t>::Deserialize(bytes, endianness, arrLength);
+
+  out.resize(arrLength);
+
+  for (std::uint64_t i = 0; i < arrLength; i++) {
+    bytes = BinarySerializer<T>::Deserialize(bytes, endianness, out[i]);
+  }
+
+  return bytes;
+}
+
+
+template<typename T, int N>
+auto BinarySerializer<Vector<T, N>>::Serialize(Vector<T, N> const& vector, std::endian const endianness, std::vector<std::uint8_t>& out) -> void {
+  for (int i = 0; i < N; i++) {
+    BinarySerializer<T>::Serialize(vector[i], endianness, out);
+  }
+}
+
+
+template<typename T, int N>
+auto BinarySerializer<Vector<T, N>>::Deserialize(std::span<std::uint8_t const> bytes, std::endian const endianness, Vector<T, N>& out) -> std::span<std::uint8_t const> {
+  for (int i = 0; i < N; i++) {
+    bytes = BinarySerializer<T>::Deserialize(bytes, endianness, out[i]);
+  }
+
+  return bytes;
+}
+
+
+namespace detail {
+[[nodiscard]] auto ReflectionSerializeToYAML(rttr::variant const& variant, std::function<YAML::Node(rttr::variant const&)> const& extensionFunc) -> YAML::Node;
+auto ReflectionDeserializeFromYAML(YAML::Node const& objNode, rttr::variant& variant, std::function<void(YAML::Node const&, rttr::variant&)> const& extensionFunc) -> void;
+}
+
+
+template<typename T>
+auto ReflectionSerializeToYAML(T const& obj, std::function<YAML::Node(rttr::variant const&)> const& extensionFunc) -> YAML::Node {
+  rttr::type const objType{ rttr::type::get(obj) };
+
+  if (!objType.is_valid()) {
+    return {};
+  }
+
+  YAML::Node retNode;
+  retNode["type"] = objType.get_name().to_string();
+
+  for (auto const prop : objType.get_properties()) {
+    retNode["properties"].push_back(detail::ReflectionSerializeToYAML(prop.get_value(obj), extensionFunc));
+  }
+
+  return retNode;
+}
+
+
+template<typename T>
+auto ReflectionDeserializeFromYAML(YAML::Node const& objNode, T& obj, std::function<void(YAML::Node const&, rttr::variant&)> const& extensionFunc) -> void {
+  auto const nodeStoredType{
+    rttr::type::get(objNode["type"]
+                      ? objNode["type"].as<std::string>()
+                      : "")
+  };
+
+  rttr::type const objType{ rttr::type::get(obj) };
+
+  if (!nodeStoredType.is_valid() || !objType.is_valid() || nodeStoredType != objType) {
+    return;
+  }
+
+  for (auto const prop : objType.get_properties()) {
+    rttr::variant propValue{ prop.get_value(obj) };
+    detail::ReflectionDeserializeFromYAML(objNode["properties"], propValue, extensionFunc);
+    prop.set_value(obj, propValue);
+  }
+}
 }
