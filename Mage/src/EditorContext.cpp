5afdb959c24daa5299a50fb2e811c2adfc6763ad
Levente Löffler <levente.loffler99@gmail.com>
2023-07-24 15:20:37 +0200

    Remove resource-management related code from EditorContext

--- Mage/src/EditorContext.cpp
+++ Mage/src/EditorContext.cpp
@@ -1,17 +1,12 @@
 #include "EditorContext.hpp"
 
-#include "Systems.hpp"
-
-#include <queue>
-#include <fstream>
-
-#include "Asset.hpp"
+#include "Platform.hpp"
 
 
 namespace sorcery::mage {
 auto Context::OnWindowFocusGain(Context* const self) -> void {
   if (!self->mProjDirAbs.empty()) {
-    self->mResourceManager.Refresh();
+    self->mResourceDB.Refresh();
   }
 }
 
@@ -47,13 +42,13 @@ auto Context::GetImGuiIo() noexcept -> ImGuiIO& {
 }
 
 
-auto Context::GetResources() const noexcept -> ResourceManager const& {
-  return mResourceManager;
+auto Context::GetResourceDatabase() const noexcept -> ResourceDB const& {
+  return mResourceDB;
 }
 
 
-auto Context::GetResources() noexcept -> ResourceManager& {
-  return mResourceManager;
+auto Context::GetResourceDatabase() noexcept -> ResourceDB& {
+  return mResourceDB;
 }
 
 
@@ -103,100 +98,16 @@ auto Context::GetProjectDirectoryAbsolute() const noexcept -> std::filesystem::p
 }
 
 
-auto Context::GetAssetDirectoryAbsolute() const noexcept -> std::filesystem::path const& {
-  return mAssetDirAbs;
-}
-
-
-auto Context::GetCacheDirectoryAbsolute() const noexcept -> std::filesystem::path const& {
-  return mCacheDirAbs;
-}
-
-
-auto Context::GetAssetDirectoryProjectRootRelative() noexcept -> std::filesystem::path const& {
-  return ASSET_DIR_REL;
-}
-
-
-auto Context::GetCacheDirectoryProjectRootRelative() noexcept -> std::filesystem::path const& {
-  return CACHE_DIR_REL;
-}
-
-
 auto Context::OpenProject(std::filesystem::path const& targetPath) -> void {
   mSelectedObject = nullptr;
   mScene = nullptr;
-  mResourceManager.Clear();
   mProjDirAbs = absolute(targetPath);
-  mAssetDirAbs = absolute(mProjDirAbs / ASSET_DIR_REL);
-  mCacheDirAbs = absolute(mProjDirAbs / CACHE_DIR_REL);
-
-  if (!exists(mAssetDirAbs)) {
-    create_directory(mAssetDirAbs);
-  }
-
-  if (!exists(mCacheDirAbs)) {
-    create_directory(mCacheDirAbs);
-  }
-
-  struct ImportInfo {
-    std::filesystem::path assetPath;
-    AssetMetaInfo metaInfo;
-  };
-
-  std::priority_queue<ImportInfo, std::vector<ImportInfo>, decltype([](ImportInfo const& lhs, ImportInfo const& rhs) {
-    return lhs.metaInfo.importPrecedence > rhs.metaInfo.importPrecedence;
-  })> queue;
-
-  for (auto const& entry : std::filesystem::recursive_directory_iterator{ mAssetDirAbs }) {
-    if (entry.path().extension() == ASSET_FILE_EXT) {
-      std::ifstream in{ entry.path(), std::ios::binary };
-      std::stringstream buffer;
-      buffer << in.rdbuf();
-      auto const assetPath{ std::filesystem::path{ entry.path() }.replace_extension() };
-      auto const meta{ ReadAssetMetaFileContents(buffer.str()) };
-      queue.emplace(assetPath, meta);
-    }
-  }
-
-  while (!queue.empty()) {
-    ImportInfo info{ queue.top() };
-    queue.pop();
-
-    auto& factory{ mWrapperManager->GetFor(info.metaInfo.type) };
-
-    auto asset{ factory.GetLoader().Load(info.assetPath, mCacheDirAbs) };
-    asset->SetName(info.assetPath.stem().string());
-    asset->SetGuid(info.metaInfo.guid);
-
-    mResourceManager.RegisterAsset(std::move(asset), info.assetPath);
-  }
+  mResourceDB.ChangeProjectDir(mProjDirAbs);
 }
 
 
 auto Context::IsEditorBusy() const noexcept -> bool {
   return mBusy;
-}
-
-
-auto Context::CreateMetaFileForRegisteredAsset(Resource const& asset) const -> void {
-  if (auto const assetPath{ mResourceManager.TryGetPathFor(&asset) }; !assetPath.empty()) {
-    std::ofstream{ std::filesystem::path{ assetPath } += ASSET_FILE_EXT } << GenerateAssetMetaFileContents(asset, *mWrapperManager);
-  }
-}
-
-
-auto Context::SaveRegisteredNativeAsset(NativeResource const& asset) const -> void {
-  if (auto const dst{ mResourceManager.TryGetPathFor(&asset) }; !dst.empty()) {
-    std::vector<std::uint8_t> static outSerializedBytes;
-    outSerializedBytes.clear();
-
-    asset.Serialize(outSerializedBytes);
-
-    if (std::ofstream out{ dst, std::ios::out | std::ios::binary }; out.is_open()) {
-      std::ranges::copy(outSerializedBytes, std::ostreambuf_iterator{ out });
-    }
-  }
 }
 
 
--- Mage/src/EditorContext.hpp
+++ Mage/src/EditorContext.hpp
@@ -2,7 +2,7 @@
 
 #include <imgui.h>
 
-#include "ResourceManager.hpp"
+#include "ResourceDB.hpp"
 #include "Scene.hpp"
 #include "ObjectWrappers/ObjectWrapperManager.hpp"
 
@@ -17,16 +17,10 @@ class Context {
   ImGuiIO& mImGuiIo;
   Scene* mScene{ nullptr };
   std::shared_ptr<ObjectWrapperManager> mWrapperManager{ ObjectWrapperManager::Create() };
-  ResourceManager mResourceManager{ mWrapperManager, this };
+  ResourceDB mResourceDB;
   Object* mSelectedObject{ nullptr };
 
-  static inline std::filesystem::path ASSET_DIR_REL{ "Assets" };
-  static inline std::filesystem::path CACHE_DIR_REL{ "Cache" };
-  static inline std::filesystem::path ASSET_FILE_EXT{ ".leopphasset" };
-
   std::filesystem::path mProjDirAbs;
-  std::filesystem::path mAssetDirAbs;
-  std::filesystem::path mCacheDirAbs;
 
   std::atomic<bool> mBusy;
 
@@ -41,8 +35,8 @@ public:
   [[nodiscard]] auto GetImGuiIo() const noexcept -> ImGuiIO const&;
   [[nodiscard]] auto GetImGuiIo() noexcept -> ImGuiIO&;
 
-  [[nodiscard]] auto GetResources() const noexcept -> ResourceManager const&;
-  [[nodiscard]] auto GetResources() noexcept -> ResourceManager&;
+  [[nodiscard]] auto GetResourceDatabase() const noexcept -> ResourceDB const&;
+  [[nodiscard]] auto GetResourceDatabase() noexcept -> ResourceDB&;
 
   [[nodiscard]] auto GetScene() const noexcept -> Scene const*;
   [[nodiscard]] auto GetScene() noexcept -> Scene*;
@@ -55,13 +49,6 @@ public:
   auto SetSelectedObject(Object* obj) noexcept -> void;
 
   [[nodiscard]] auto GetProjectDirectoryAbsolute() const noexcept -> std::filesystem::path const&;
-  [[nodiscard]] auto GetAssetDirectoryAbsolute() const noexcept -> std::filesystem::path const&;
-  [[nodiscard]] auto GetCacheDirectoryAbsolute() const noexcept -> std::filesystem::path const&;
-
-  [[nodiscard]] static auto GetAssetDirectoryProjectRootRelative() noexcept -> std::filesystem::path const&;
-  [[nodiscard]] static auto GetCacheDirectoryProjectRootRelative() noexcept -> std::filesystem::path const&;
-
-  [[nodiscard]] inline static auto GetAssetFileExtension() noexcept -> std::filesystem::path const&;
 
   auto OpenProject(std::filesystem::path const& targetPath) -> void;
 
@@ -69,9 +56,6 @@ public:
 
   template<typename Callable>
   auto ExecuteInBusyEditor(Callable&& callable) -> void;
-
-  auto CreateMetaFileForRegisteredAsset(Resource const& asset) const -> void;
-  auto SaveRegisteredNativeAsset(NativeResource const& asset) const -> void;
 
 
   struct BusyExecutionContext {
@@ -101,10 +85,5 @@ auto Context::ExecuteInBusyEditor(Callable&& callable) -> void {
       OnFinishBusyExecution(execContext);
     }
   }.detach();
-}
-
-
-inline auto Context::GetAssetFileExtension() noexcept -> std::filesystem::path const& {
-  return ASSET_FILE_EXT;
 }
 }
