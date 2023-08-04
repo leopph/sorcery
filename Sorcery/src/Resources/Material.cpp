bbccf4b19e02a768df382be979d5532a2d277962
Levente Löffler <levente.loffler99@gmail.com>
2023-08-04 11:39:30 +0200

    Update Material::OnDrawProperties to use ResourcePicker

--- Sorcery/src/Resources/Material.cpp
+++ Sorcery/src/Resources/Material.cpp
@@ -3,16 +3,16 @@
 #include "../Renderer.hpp"
 #include "../Serialization.hpp"
 #undef FindResource
+#include "../GUI.hpp"
 #include "../ResourceManager.hpp"
 
 #include <cstdint>
 #include <imgui.h>
-#include <imgui_stdlib.h>
 #include <stdexcept>
 
 
 RTTR_REGISTRATION {
-  rttr::registration::class_<sorcery::Material>{ "Material" };
+  rttr::registration::class_<sorcery::Material>{"Material"};
 }
 
 
@@ -42,7 +42,7 @@ auto Material::CreateCB() -> void {
   };
 
   if (FAILED(gRenderer.GetDevice()->CreateBuffer(&cbDesc, &initialData, mCB.GetAddressOf()))) {
-    throw std::runtime_error{ "Failed to create material CB." };
+    throw std::runtime_error{"Failed to create material CB."};
   }
 }
 
@@ -64,13 +64,13 @@ auto Material::SetAlbedoVector(Vector3 const& albedoVector) noexcept -> void {
 
 
 auto Material::GetAlbedoColor() const noexcept -> Color {
-  auto const mulColorVec{ GetAlbedoVector() * 255 };
-  return Color{ static_cast<u8>(mulColorVec[0]), static_cast<u8>(mulColorVec[1]), static_cast<u8>(mulColorVec[2]), static_cast<u8>(mulColorVec[3]) };
+  auto const mulColorVec{GetAlbedoVector() * 255};
+  return Color{static_cast<u8>(mulColorVec[0]), static_cast<u8>(mulColorVec[1]), static_cast<u8>(mulColorVec[2]), static_cast<u8>(mulColorVec[3])};
 }
 
 
 auto Material::SetAlbedoColor(Color const& albedoColor) noexcept -> void {
-  SetAlbedoVector(Vector3{ static_cast<float>(albedoColor.red) / 255.f, static_cast<float>(albedoColor.green) / 255.f, static_cast<float>(albedoColor.blue) / 255.f });
+  SetAlbedoVector(Vector3{static_cast<float>(albedoColor.red) / 255.f, static_cast<float>(albedoColor.green) / 255.f, static_cast<float>(albedoColor.blue) / 255.f});
 }
 
 
@@ -180,27 +180,27 @@ auto Material::Serialize() const noexcept -> YAML::Node {
   ret["roughness"] = GetRoughness();
   ret["ao"] = GetAo();
 
-  auto const albedoMap{ GetAlbedoMap() };
+  auto const albedoMap{GetAlbedoMap()};
   ret["albedoMap"] = albedoMap
                        ? albedoMap->GetGuid()
                        : Guid::Invalid();
 
-  auto const metallicMap{ GetMetallicMap() };
+  auto const metallicMap{GetMetallicMap()};
   ret["metallicMap"] = metallicMap
                          ? metallicMap->GetGuid()
                          : Guid::Invalid();
 
-  auto const roughnessMap{ GetRoughnessMap() };
+  auto const roughnessMap{GetRoughnessMap()};
   ret["roughnessMap"] = roughnessMap
                           ? roughnessMap->GetGuid()
                           : Guid::Invalid();
 
-  auto const aoMap{ GetAoMap() };
+  auto const aoMap{GetAoMap()};
   ret["aoMap"] = aoMap
                    ? aoMap->GetGuid()
                    : Guid::Invalid();
 
-  auto const normalMap{ GetNormalMap() };
+  auto const normalMap{GetNormalMap()};
   ret["normalMap"] = normalMap
                        ? normalMap->GetGuid()
                        : Guid::Invalid();
@@ -215,27 +215,27 @@ auto Material::Deserialize(YAML::Node const& yamlNode) noexcept -> void {
   SetRoughness(yamlNode["roughness"].as<float>(GetRoughness()));
   SetAo(yamlNode["ao"].as<float>(GetAo()));
 
-  auto const albedoMapGuid{ yamlNode["albedoMap"].as<Guid>() };
+  auto const albedoMapGuid{yamlNode["albedoMap"].as<Guid>()};
   SetAlbedoMap(albedoMapGuid.IsValid()
                  ? gResourceManager.Load<Texture2D>(albedoMapGuid)
                  : GetAlbedoMap());
 
-  auto const metallicMapGuid{ yamlNode["metallicMap"].as<Guid>() };
+  auto const metallicMapGuid{yamlNode["metallicMap"].as<Guid>()};
   SetMetallicMap(metallicMapGuid.IsValid()
                    ? gResourceManager.Load<Texture2D>(metallicMapGuid)
                    : GetMetallicMap());
 
-  auto const roughnessMapGuid{ yamlNode["roughnessMap"].as<Guid>() };
+  auto const roughnessMapGuid{yamlNode["roughnessMap"].as<Guid>()};
   SetRoughnessMap(roughnessMapGuid.IsValid()
                     ? gResourceManager.Load<Texture2D>(roughnessMapGuid)
                     : GetRoughnessMap());
 
-  auto const aoMapGuid{ yamlNode["aoMap"].as<Guid>() };
+  auto const aoMapGuid{yamlNode["aoMap"].as<Guid>()};
   SetAoMap(aoMapGuid.IsValid()
              ? gResourceManager.Load<Texture2D>(aoMapGuid)
              : GetAoMap());
 
-  auto const normalMapGuid{ yamlNode["normalMap"].as<Guid>() };
+  auto const normalMapGuid{yamlNode["normalMap"].as<Guid>()};
   SetNormalMap(normalMapGuid.IsValid()
                  ? gResourceManager.Load<Texture2D>(normalMapGuid)
                  : GetNormalMap());
@@ -256,7 +256,7 @@ auto Material::OnDrawProperties(bool& changed) -> void {
     ImGui::Text("Albedo Color");
     ImGui::TableNextColumn();
 
-    if (Vector3 albedoColor{ GetAlbedoVector() }; ImGui::ColorEdit3("###matAlbedoColor", albedoColor.GetData())) {
+    if (Vector3 albedoColor{GetAlbedoVector()}; ImGui::ColorEdit3("###matAlbedoColor", albedoColor.GetData())) {
       SetAlbedoVector(albedoColor);
       changed = true;
     }
@@ -265,7 +265,7 @@ auto Material::OnDrawProperties(bool& changed) -> void {
     ImGui::Text("Metallic");
     ImGui::TableNextColumn();
 
-    if (f32 metallic{ GetMetallic() }; ImGui::SliderFloat("###matMetallic", &metallic, 0.0f, 1.0f)) {
+    if (f32 metallic{GetMetallic()}; ImGui::SliderFloat("###matMetallic", &metallic, 0.0f, 1.0f)) {
       SetMetallic(metallic);
       changed = true;
     }
@@ -274,7 +274,7 @@ auto Material::OnDrawProperties(bool& changed) -> void {
     ImGui::Text("Roughness");
     ImGui::TableNextColumn();
 
-    if (f32 roughness{ GetRoughness() }; ImGui::SliderFloat("###matRoughness", &roughness, 0.0f, 1.0f)) {
+    if (f32 roughness{GetRoughness()}; ImGui::SliderFloat("###matRoughness", &roughness, 0.0f, 1.0f)) {
       SetRoughness(roughness);
       changed = true;
     }
@@ -283,114 +283,50 @@ auto Material::OnDrawProperties(bool& changed) -> void {
     ImGui::Text("Ambient Occlusion");
     ImGui::TableNextColumn();
 
-    if (f32 ao{ GetAo() }; ImGui::SliderFloat("###matAo", &ao, 0.0f, 1.0f)) {
+    if (f32 ao{GetAo()}; ImGui::SliderFloat("###matAo", &ao, 0.0f, 1.0f)) {
       SetAo(ao);
       changed = true;
     }
 
-    auto drawTextureOption{
-      [callCount = 0, &changed]<typename TexGetFn, typename TexSetFn>(std::string_view const label, TexGetFn&& texGetFn, TexSetFn&& texSetFn) mutable requires std::is_invocable_r_v<ObserverPtr<Texture2D>, TexGetFn> && std::is_invocable_r_v<void, TexSetFn, ObserverPtr<Texture2D>> {
-        auto constexpr nullTexName{ "None" };
-        static std::vector<ObserverPtr<Texture2D>> textures;
-        static std::string texFilter;
+    ImGui::TableNextColumn();
+    ImGui::Text("%s", "Albedo Map");
+    ImGui::TableNextColumn();
+    static ResourcePicker<Texture2D> albedoMapPicker;
+    if (auto albedoMap{GetAlbedoMap()}; albedoMapPicker.Draw(albedoMap)) {
+      SetAlbedoMap(albedoMap);
+    }
 
-        ImGui::TableNextColumn();
-        ImGui::Text("%s", label.data());
-        ImGui::TableNextColumn();
+    ImGui::TableNextColumn();
+    ImGui::Text("%s", "Metallic Map");
+    ImGui::TableNextColumn();
+    static ResourcePicker<Texture2D> metallicMapPicker;
+    if (auto metallicMap{GetMetallicMap()}; metallicMapPicker.Draw(metallicMap)) {
+      SetMetallicMap(metallicMap);
+    }
 
-        auto const popupId{ std::format("SelectTexturePopUp{}", callCount) };
+    ImGui::TableNextColumn();
+    ImGui::Text("%s", "Roughness Map");
+    ImGui::TableNextColumn();
+    static ResourcePicker<Texture2D> roughnessMapPicker;
+    if (auto roughnessMap{GetRoughnessMap()}; roughnessMapPicker.Draw(roughnessMap)) {
+      SetRoughnessMap(roughnessMap);
+    }
 
-        auto const queryTextures{
-          [] {
-            FindObjectsOfType(textures);
+    ImGui::TableNextColumn();
+    ImGui::Text("%s", "Ambient Occlusion Map");
+    ImGui::TableNextColumn();
+    static ResourcePicker<Texture2D> aoMapPicker;
+    if (auto aoMap{GetAoMap()}; aoMapPicker.Draw(aoMap)) {
+      SetAoMap(aoMap);
+    }
 
-            std::erase_if(textures, [](auto const tex) {
-              return tex && !Contains(tex->GetName(), texFilter);
-            });
-
-            std::ranges::sort(textures, [](auto const lhs, auto const rhs) {
-              return !lhs || (rhs && lhs->GetName() < rhs->GetName());
-            });
-
-            textures.insert(std::begin(textures), nullptr);
-          }
-        };
-
-        if (ImGui::BeginPopup(popupId.data())) {
-          if (ImGui::InputText("###SearchTextures", &texFilter)) {
-            queryTextures();
-          }
-
-          for (auto const tex : textures) {
-            if (ImGui::Selectable(std::format("{}##texoption{}", tex
-                                                                   ? tex->GetName()
-                                                                   : nullTexName, tex
-                                                                                    ? tex->GetGuid().ToString()
-                                                                                    : "0").c_str(), tex == texGetFn())) {
-              texSetFn(tex);
-              changed = true;
-
-              break;
-            }
-          }
-
-          ImGui::EndPopup();
-        }
-
-        if (ImGui::Button(std::format("Select##SelectTextureButton{}", callCount).data())) {
-          texFilter.clear();
-          queryTextures();
-          ImGui::OpenPopup(popupId.data());
-        }
-
-        ImGui::SameLine();
-        ImGui::Text("%s", texGetFn()
-                            ? texGetFn()->GetName().data()
-                            : nullTexName);
-
-        callCount += 1;
-      }
-    };
-
-    drawTextureOption("Albedo Map",
-                      [this] {
-                        return GetAlbedoMap();
-                      },
-                      [this](auto const tex) -> void {
-                        SetAlbedoMap(tex);
-                      });
-
-    drawTextureOption("Metallic Map",
-                      [this] {
-                        return GetMetallicMap();
-                      },
-                      [this](auto const tex) -> void {
-                        SetMetallicMap(tex);
-                      });
-
-    drawTextureOption("Roughness Map",
-                      [this] {
-                        return GetRoughnessMap();
-                      },
-                      [this](auto const tex) -> void {
-                        SetRoughnessMap(tex);
-                      });
-
-    drawTextureOption("Ambient Occlusion Map",
-                      [this] {
-                        return GetAoMap();
-                      },
-                      [this](auto const tex) -> void {
-                        SetAoMap(tex);
-                      });
-
-    drawTextureOption("Normal Map",
-                      [this] {
-                        return GetNormalMap();
-                      },
-                      [this](auto const tex) -> void {
-                        SetNormalMap(tex);
-                      });
+    ImGui::TableNextColumn();
+    ImGui::Text("%s", "Normal Map");
+    ImGui::TableNextColumn();
+    static ResourcePicker<Texture2D> normalMapPicker;
+    if (auto normalMap{GetNormalMap()}; normalMapPicker.Draw(normalMap)) {
+      SetNormalMap(normalMap);
+    }
 
     ImGui::EndTable();
   }
