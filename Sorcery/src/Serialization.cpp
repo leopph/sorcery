10e3a609081989bf7c8b46fd28b52efdbf4b3506
Levente Löffler <levente.loffler99@gmail.com>
2023-08-17 16:03:09 +0200

    Fix conversion from Resource* to property type when deserializing Resource pointer in ReflectionDeserializeFromYaml

--- Sorcery/src/Serialization.cpp
+++ Sorcery/src/Serialization.cpp
@@ -330,11 +330,12 @@ auto ReflectionDeserializeFromYaml(YAML::Node const& node, rttr::variant& v, std
 
   if (v.get_type().is_pointer() && v.get_type().get_raw_type().is_derived_from(rttr::type::get<Resource>())) {
     try {
-      if (auto const res{gResourceManager.GetOrLoad(node.as<Guid>())}; res && v.can_convert(rttr::type::get(res))) {
-        auto const type{v.get_type()};
-        v = res;
-        [[maybe_unused]] auto const success{v.convert(type)};
-        assert(success);
+      if (auto const res{gResourceManager.GetOrLoad(node.as<Guid>())}) {
+        if (rttr::variant resVar{res}; resVar.can_convert(v.get_type())) {
+          [[maybe_unused]] auto const success{resVar.convert(v.get_type())};
+          assert(success);
+          v = resVar;
+        }
       }
     } catch (...) { }
     return;
