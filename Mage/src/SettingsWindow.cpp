49621031e3bdb1cb0b304d868427b3f4d17554be
Levente Löffler <levente.loffler99@gmail.com>
2024-02-18 22:48:40 +0100

    Remove mention of PCSS

--- Mage/src/SettingsWindow.cpp
+++ Mage/src/SettingsWindow.cpp
@@ -16,9 +16,7 @@ SettingsWindow::SettingsWindow(Application& app, StandaloneCamera& sceneViewCam)
 
 
 auto SettingsWindow::Draw() -> void {
-  ImGui::SetNextWindowSizeConstraints(ImVec2{200, 200}, ImVec2{
-    std::numeric_limits<float>::max(), std::numeric_limits<float>::max()
-  });
+  ImGui::SetNextWindowSizeConstraints(ImVec2{200, 200}, ImVec2{std::numeric_limits<float>::max(), std::numeric_limits<float>::max()});
 
   if (std::pmr::string windowName{&GetTmpMemRes()}; !ImGui::Begin(windowName.append(TITLE).append("##Window").c_str())) {
     ImGui::End();
@@ -40,15 +38,12 @@ auto SettingsWindow::Draw() -> void {
     }
 
     ImGui::BeginDisabled(!isFrameRateLimited);
-    if (int targetFrameRate{timing::GetTargetFrameRate()}; ImGui::DragInt("Target Frame Rate", &targetFrameRate, 1, 30,
-      std::numeric_limits<int>::max(), "%d", ImGuiSliderFlags_AlwaysClamp)) {
+    if (int targetFrameRate{timing::GetTargetFrameRate()}; ImGui::DragInt("Target Frame Rate", &targetFrameRate, 1, 30, std::numeric_limits<int>::max(), "%d", ImGuiSliderFlags_AlwaysClamp)) {
       timing::SetTargetFrameRate(targetFrameRate);
     }
     ImGui::EndDisabled();
 
-    if (int inFlightFrameCount{gRenderer.GetInFlightFrameCount()}; ImGui::SliderInt("In-Flight Frame Count",
-      &inFlightFrameCount, Renderer::MIN_IN_FLIGHT_FRAME_COUNT, Renderer::MAX_IN_FLIGHT_FRAME_COUNT, "%d",
-      ImGuiSliderFlags_AlwaysClamp)) {
+    if (int inFlightFrameCount{gRenderer.GetInFlightFrameCount()}; ImGui::SliderInt("In-Flight Frame Count", &inFlightFrameCount, Renderer::MIN_IN_FLIGHT_FRAME_COUNT, Renderer::MAX_IN_FLIGHT_FRAME_COUNT, "%d", ImGuiSliderFlags_AlwaysClamp)) {
       gRenderer.SetInFlightFrameCount(inFlightFrameCount);
     }
 
@@ -62,8 +57,7 @@ auto SettingsWindow::Draw() -> void {
 
     constexpr char const* msaaComboLabels[]{"Off", "2x", "4x", "8x"};
 
-    if (auto const msaaModeIdx{static_cast<int>(std::log2(static_cast<int>(gRenderer.GetMultisamplingMode())))};
-      ImGui::BeginCombo("MSAA", msaaComboLabels[msaaModeIdx])) {
+    if (auto const msaaModeIdx{static_cast<int>(std::log2(static_cast<int>(gRenderer.GetMultisamplingMode())))}; ImGui::BeginCombo("MSAA", msaaComboLabels[msaaModeIdx])) {
       for (auto i{0}; i < 4; i++) {
         if (ImGui::Selectable(msaaComboLabels[i], msaaModeIdx == i)) {
           gRenderer.SetMultisamplingMode(static_cast<Renderer::MultisamplingMode>(static_cast<int>(std::pow(2, i))));
@@ -118,31 +112,27 @@ auto SettingsWindow::Draw() -> void {
 
   if (ImGui::TreeNode("Shadows")) {
     float shadowDistance{gRenderer.GetShadowDistance()};
-    if (ImGui::DragFloat("Shadow Distance", &shadowDistance, 1, 0, std::numeric_limits<float>::max(), "%.0f",
-      ImGuiSliderFlags_AlwaysClamp)) {
+    if (ImGui::DragFloat("Shadow Distance", &shadowDistance, 1, 0, std::numeric_limits<float>::max(), "%.0f", ImGuiSliderFlags_AlwaysClamp)) {
       gRenderer.SetShadowDistance(shadowDistance);
     }
 
     auto constexpr shadowFilteringModeNames{
       [] {
-        std::array<char const*, 6> ret{};
+        std::array<char const*, 5> ret{};
         ret[static_cast<int>(Renderer::ShadowFilteringMode::None)] = "No Filtering";
         ret[static_cast<int>(Renderer::ShadowFilteringMode::HardwarePCF)] = "PCF 2x2 (hardware)";
         ret[static_cast<int>(Renderer::ShadowFilteringMode::PCF3x3)] = "PCF 3x3 (4 taps)";
         ret[static_cast<int>(Renderer::ShadowFilteringMode::PCFTent3x3)] = "PCF Tent 3x3 (4 taps)";
         ret[static_cast<int>(Renderer::ShadowFilteringMode::PCFTent5x5)] = "PCF Tent 5x5 (9 taps)";
-        ret[static_cast<int>(Renderer::ShadowFilteringMode::PCSS)] = "PCSS (Not yet implemented)";
         return ret;
       }()
     };
 
-    if (int currentShadowFilteringModeIdx{static_cast<int>(gRenderer.GetShadowFilteringMode())};
-      ImGui::Combo("Shadow Filtering Mode", &currentShadowFilteringModeIdx, shadowFilteringModeNames.data(), static_cast<int>(std::ssize(shadowFilteringModeNames)))) {
+    if (int currentShadowFilteringModeIdx{static_cast<int>(gRenderer.GetShadowFilteringMode())}; ImGui::Combo("Shadow Filtering Mode", &currentShadowFilteringModeIdx, shadowFilteringModeNames.data(), static_cast<int>(std::ssize(shadowFilteringModeNames)))) {
       gRenderer.SetShadowFilteringMode(static_cast<Renderer::ShadowFilteringMode>(currentShadowFilteringModeIdx));
     }
 
-    if (int cascadeCount{gRenderer.GetShadowCascadeCount()}; ImGui::SliderInt("Shadow Cascade Count", &cascadeCount, 1,
-      Renderer::GetMaxShadowCascadeCount(), "%d", ImGuiSliderFlags_NoInput)) {
+    if (int cascadeCount{gRenderer.GetShadowCascadeCount()}; ImGui::SliderInt("Shadow Cascade Count", &cascadeCount, 1, Renderer::GetMaxShadowCascadeCount(), "%d", ImGuiSliderFlags_NoInput)) {
       gRenderer.SetShadowCascadeCount(cascadeCount);
     }
 
@@ -150,14 +140,12 @@ auto SettingsWindow::Draw() -> void {
     auto const splitCount{std::ssize(cascadeSplits)};
 
     for (int i = 0; i < splitCount; i++) {
-      if (float cascadeSplit{cascadeSplits[i] * 100.0f}; ImGui::SliderFloat(
-        std::format("Split {} (percent)", i + 1).data(), &cascadeSplit, 0, 100, "%.3f", ImGuiSliderFlags_NoInput)) {
+      if (float cascadeSplit{cascadeSplits[i] * 100.0f}; ImGui::SliderFloat(std::format("Split {} (percent)", i + 1).data(), &cascadeSplit, 0, 100, "%.3f", ImGuiSliderFlags_NoInput)) {
         gRenderer.SetNormalizedShadowCascadeSplit(i, cascadeSplit / 100.0f);
       }
     }
 
-    if (bool visualizeShadowCascades{gRenderer.IsVisualizingShadowCascades()}; ImGui::Checkbox(
-      "Visualize Shadow Cascades", &visualizeShadowCascades)) {
+    if (bool visualizeShadowCascades{gRenderer.IsVisualizingShadowCascades()}; ImGui::Checkbox("Visualize Shadow Cascades", &visualizeShadowCascades)) {
       gRenderer.VisualizeShadowCascades(visualizeShadowCascades);
     }
     ImGui::TreePop();
