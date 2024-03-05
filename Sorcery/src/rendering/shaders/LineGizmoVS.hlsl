c13fea25883a2fb2f46a35e77415ced1ffbdbe3e
Levente Löffler <levente.loffler99@gmail.com>
2024-03-05 10:18:22 +0100

    Rewrite LineGizmoVS to SM6.6

--- Sorcery/src/rendering/shaders/LineGizmoVS.hlsl
+++ Sorcery/src/rendering/shaders/LineGizmoVS.hlsl
@@ -1,13 +1,18 @@
 #include "ShaderInterop.h"
 #include "GizmoVsOut.hlsli"
 
-STRUCTUREDBUFFER(ShaderLineGizmoVertexSB, ShaderLineGizmoVertexData, RES_SLOT_LINE_GIZMO_VERTEX);
+struct DrawParams {
+  uint vertex_buf_idx;
+};
 
+ConstantBuffer<DrawParams> g_draw_params : register(b0, space0);
 
-GizmoVsOut main(const uint instanceId : SV_InstanceID, const uint vertexId : SV_VertexID) {
-    const ShaderLineGizmoVertexData data = ShaderLineGizmoVertexSB[instanceId];
-    GizmoVsOut ret;
-    ret.colorIdx = data.colorIdx;
-    ret.position = mul(float4(vertexId == 0 ? data.from : data.to, 1), gPerViewConstants.viewProjMtx);
-    return ret;
+GizmoVsOut main(const uint vertex_id : SV_VertexID, const uint instance_id : SV_InstanceID) {
+  const StructuredBuffer<ShaderLineGizmoVertexData> vertices = ResourceDescriptorHeap[g_draw_params.vertex_buf_idx];
+  const ShaderLineGizmoVertexData data = vertices[instance_id];
+  
+  GizmoVsOut ret;
+  ret.colorIdx = data.colorIdx;
+  ret.position = mul(float4(vertex_id == 0 ? data.from : data.to, 1), gPerViewConstants.viewProjMtx);
+  return ret;
 }
