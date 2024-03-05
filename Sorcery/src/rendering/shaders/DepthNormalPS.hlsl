d75ace5a5fba7ae4cb2c70224e38157fd8855a1a
Levente Löffler <levente.loffler99@gmail.com>
2024-03-05 13:10:41 +0100

    Rewrite DepthNormalPS to SM6.6

--- Sorcery/src/rendering/shaders/DepthNormalPS.hlsl
+++ Sorcery/src/rendering/shaders/DepthNormalPS.hlsl
@@ -1,19 +1,32 @@
 #include "ShaderInterop.h"
 #include "DepthNormalVsOut.hlsli"
-#include "Samplers.hlsli"
 
-TEXTURE2D(gOpacityMask, float, RES_SLOT_OPACITY_MASK);
-TEXTURE2D(gNormalMap, float3, RES_SLOT_NORMAL_MAP);
+struct DrawParams {
+  uint mtl_idx;
+  uint samp_idx;
+};
 
-float3 main(const DepthNormalVsOut vsOut) : SV_TARGET {
-  if (material.blendMode == BLEND_MODE_ALPHA_CLIP && material.sampleOpacityMap && gOpacityMask.Sample(gSamplerAf16Wrap, vsOut.uv) < material.alphaThreshold) {
-    discard;
+ConstantBuffer<DrawParams> g_draw_params : register(b0, space0);
+
+float3 main(const DepthNormalVsOut vs_out) : SV_Target {
+  const ConstantBuffer<ShaderMaterial> mtl = ResourceDescriptorHeap[g_draw_params.mtl_idx];
+
+  if (mtl.blendMode == BLEND_MODE_ALPHA_CLIP && mtl.opacity_map_idx != INVALID_RES_IDX) {
+    const Texture2D<float> opacity_map = ResourceDescriptorHeap[mtl.opacity_map_idx];
+    const SamplerState samp = SamplerDescriptorHeap[g_draw_params.samp_idx];
+
+    if (opacity_map.Sample(samp, vs_out.uv) < mtl.alphaThreshold) {
+      discard;
+    }
   }
 
-  if (material.sampleNormal) {
-    const float3 normalTS = gNormalMap.Sample(gSamplerAf16Wrap, vsOut.uv).rgb * 2.0 - 1.0;
-    return normalize(mul(normalTS, vsOut.tbnMtxWS));
+  if (mtl.normal_map_idx != INVALID_RES_IDX) {
+    const Texture2D<float3> normal_map = ResourceDescriptorHeap[mtl.normal_map_idx];
+    const SamplerState samp = SamplerDescriptorHeap[g_draw_params.samp_idx];
+
+    const float3 normal_ts = normal_map.Sample(samp, vs_out.uv).rgb * 2.0 - 1.0;
+    return normalize(mul(normal_ts, vs_out.tbnMtxWS));
   }
 
-  return normalize(vsOut.normalWS);
+  return normalize(vs_out.normalWS);
 }
