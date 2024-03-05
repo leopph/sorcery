c68f0453c1bb2b953b0cf891fcb4d8a372dcfd08
Levente Löffler <levente.loffler99@gmail.com>
2024-03-05 12:39:37 +0100

    Rewrite SsaoBlurPS to SM6.6

--- Sorcery/src/rendering/shaders/SsaoBlurPS.hlsl
+++ Sorcery/src/rendering/shaders/SsaoBlurPS.hlsl
@@ -1,22 +1,30 @@
 #include "ShaderInterop.h"
-#include "Samplers.hlsli"
 
-TEXTURE2D(gSsaoInput, float, RES_SLOT_SSAO_BLUR_INPUT);
+struct DrawParams {
+  uint in_tex_idx;
+  uint point_clamp_samp_idx;
+};
 
-float main(const float4 pixelCoord : SV_POSITION, const float2 uv : TEXCOORD) : SV_TARGET {
-  uint2 textureSize;
-  gSsaoInput.GetDimensions(textureSize.x, textureSize.y);
-  const float2 texelSize = 1.0 / float2(textureSize);
+ConstantBuffer<DrawParams> g_draw_params : register(b0, space0);
+
+float main(const float4 pixel_coord : SV_POSITION, const float2 uv : TEXCOORD) : SV_TARGET {
+  uint2 texture_size;
+
+  const Texture2D<float> in_tex = ResourceDescriptorHeap[g_draw_params.in_tex_idx];
+  in_tex.GetDimensions(texture_size.x, texture_size.y);
+  const float2 texel_size = 1.0 / float2(texture_size);
 
   float result = 0;
 
   const int lo = -SSAO_NOISE_TEX_DIM / 2;
   const int hi = SSAO_NOISE_TEX_DIM / 2;
 
+  const SamplerState point_clamp_samp = SamplerDescriptorHeap[g_draw_params.point_clamp_samp_idx];
+
   for (int x = lo; x < hi; x++) {
     for (int y = lo; y < hi; y++) {
-      const float2 uvOffset = float2(x, y) * texelSize;
-      result += gSsaoInput.Sample(gSamplerPointClamp, uv + uvOffset).r;
+      const float2 uv_offset = float2(x, y) * texel_size;
+      result += in_tex.Sample(point_clamp_samp, uv + uv_offset).r;
     }
   }
 
