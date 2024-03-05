34a264ec5ccc169cf76c3efc9c1ac5d632c4d046
Levente Löffler <levente.loffler99@gmail.com>
2024-03-05 10:26:33 +0100

    Rewrite ScreenVS to SM6.6

--- Sorcery/src/rendering/shaders/ScreenVS.hlsl
+++ Sorcery/src/rendering/shaders/ScreenVS.hlsl
@@ -1,9 +1,9 @@
 #include "ScreenVsOut.hlsli"
 #include "Util.hlsli"
 
-ScreenVsOut main(const uint vertexId : SV_VertexID) {
+ScreenVsOut main(const uint vertex_id : SV_VertexID) {
 	ScreenVsOut ret;
-	ret.uv = float2((vertexId << 1) & 2, vertexId & 2);
+	ret.uv = float2((vertex_id << 1) & 2, vertex_id & 2);
 	ret.positionCS = float4(UvToNdc(ret.uv), 0, 1);
 	return ret;
 }
