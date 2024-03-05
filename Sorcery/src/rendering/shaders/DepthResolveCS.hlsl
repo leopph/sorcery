e40785d83e421fa69cebe44e23b889d8527a3aad
Levente Löffler <levente.loffler99@gmail.com>
2024-03-05 10:36:29 +0100

    Rewrite DepthResolveCS to SM6.6

--- Sorcery/src/rendering/shaders/DepthResolveCS.hlsl
+++ Sorcery/src/rendering/shaders/DepthResolveCS.hlsl
@@ -1,29 +1,39 @@
 #include "ShaderInterop.h"
 
-TEXTURE2DMS(gInput, float, RES_SLOT_DEPTH_RESOLVE_INPUT);
-RWTEXTURE2D(gOutput, float, UAV_SLOT_DEPTH_RESOLVE_OUTPUT);
+struct DispatchParams {
+  uint in_tex_idx;
+  uint out_tex_idx;
+  uint per_frame_cb_idx;
+};
+
+ConstantBuffer<DispatchParams> g_dispatch_params : register(b0, space0);
  
 [numthreads(DEPTH_RESOLVE_CS_THREADS_X, DEPTH_RESOLVE_CS_THREADS_X, DEPTH_RESOLVE_CS_THREADS_Z)]
-void main(const uint3 dispatchThreadId : SV_DispatchThreadID) {
-  uint2 texSize;
-  uint sampleCount;
-  gInput.GetDimensions(texSize.x, texSize.y, sampleCount);
+void main(const uint3 dtid : SV_DispatchThreadID) {
+  uint2 tex_size;
+  uint sample_count;
+
+  const Texture2DMS<float> in_tex = ResourceDescriptorHeap[g_dispatch_params.in_tex_idx];
+  in_tex.GetDimensions(tex_size.x, tex_size.y, sample_count);
    
-  if (dispatchThreadId.x > texSize.x || dispatchThreadId.y > texSize.y) {
+  if (dtid.x > tex_size.x || dtid.y > tex_size.y) {
     return;
   }
-   
-  float result = 1 - gPerFrameConstants.isUsingReversedZ;
 
-  if (gPerFrameConstants.isUsingReversedZ) {
-    for (uint i = 0; i < sampleCount; ++i) {
-      result = max(result, gInput.Load(dispatchThreadId.xy, i).r);
+  const ConstantBuffer<ShaderPerFrameConstants> per_frame_cb = ResourceDescriptorHeap[g_dispatch_params.per_frame_cb_idx];
+  float result = 1 - per_frame_cb.isUsingReversedZ;
+
+  const RWTexture2D<float> out_tex = ResourceDescriptorHeap[g_dispatch_params.out_tex_idx];
+
+  if (per_frame_cb.isUsingReversedZ) {
+    for (uint i = 0; i < sample_count; ++i) {
+      result = max(result, in_tex.Load(dtid.xy, i).r);
     }
   } else {
-    for (uint i = 0; i < sampleCount; ++i) {
-      result = min(result, gInput.Load(dispatchThreadId.xy, i).r);
+    for (uint i = 0; i < sample_count; ++i) {
+      result = min(result, in_tex.Load(dtid.xy, i).r);
     }
   }
    
-  gOutput[dispatchThreadId.xy] = result;
+  out_tex[dtid.xy] = result;
 }
