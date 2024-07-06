3aa2dc0fcbae0306dbfd392938c3a8fc098b4d69
Levente Löffler <levente.loffler99@gmail.com>
2024-07-06 15:01:40 +0200

    Add reflection and ctors to Behavior

---
+++ Sorcery/src/scene_objects/Behavior.cpp
@@ -0,0 +1,5 @@
+#include "Behavior.hpp"
+
+RTTR_REGISTRATION {
+  rttr::registration::class_<sorcery::Behavior>{"Behavior"};
+}
--- Sorcery/src/scene_objects/Behavior.hpp
+++ Sorcery/src/scene_objects/Behavior.hpp
@@ -8,6 +8,17 @@ class Behavior : public virtual Component {
   RTTR_ENABLE(Component)
 
 public:
-  virtual void Update() {}
+  virtual auto Update() -> void {}
+
+protected:
+  Behavior() = default;
+  Behavior(Behavior const& other) = default;
+  Behavior(Behavior&& other) noexcept = default;
+
+public:
+  ~Behavior() override = default;
+
+  auto operator=(Behavior const& other) -> void = delete;
+  auto operator=(Behavior&& other) -> void = delete;
 };
 }
