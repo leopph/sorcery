0d13586237a3dc7faef5a92e528bc61414746d7c
Levente Löffler <levente.loffler99@gmail.com>
2025-02-22 22:05:42 +0100

    Remove meshlet vert/prim count requirement

--- Sorcery/src/mesh_data.cpp
+++ Sorcery/src/mesh_data.cpp
@@ -6,5 +6,4 @@
 namespace sorcery {
 std::uint16_t const kMeshletMaxVerts{MESHLET_MAX_VERTS};
 std::uint16_t const kMeshletMaxPrims{MESHLET_MAX_PRIMS};
-static_assert(kMeshletMaxVerts < kMeshletMaxPrims, "Meshlet max vertex count must be less than max primitive count!");
 }
